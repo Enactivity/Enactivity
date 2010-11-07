9ca8e74edc1d80fa7139c1fb30f6a2d1f2aa8a2c
ajsharma <ajsharma@f94a5964-8f9d-401d-8ecb-23302dea6c3a>
2010-11-07 03:55:25 +0000

    Adding favicon to default layout

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -14,6 +14,8 @@
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
 
+	<link rel="shortcut icon" href="<?php echo Yii::app()->request->baseUrl; ?>/images/favicon.ico"/> 
+
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
 </head>
 
