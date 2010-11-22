5bcbee222af4bd6f41f625d7c05d7bcc5766770c
ajsharma <ajsharma@f94a5964-8f9d-401d-8ecb-23302dea6c3a>
2010-11-22 05:35:57 +0000

    Replaced <i> with <em>

--- yii_framework/cli/views/webapp/protected/views/site/index.php
+++ yii_framework/cli/views/webapp/protected/views/site/index.php
@@ -1,6 +1,6 @@
 <?php $this->pageTitle=Yii::app()->name; ?>
 
-<h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
+<h1>Welcome to <em><?php echo CHtml::encode(Yii::app()->name); ?></em></h1>
 
 <p>Congratulations! You have successfully created your Yii application.</p>
 
