8272ca08157bc1ba5f9408c95d2521154558f7ce
ajsharma <ajsharma@f94a5964-8f9d-401d-8ecb-23302dea6c3a>
2010-12-19 01:39:22 +0000

    Removed menu items from viewing user to avoid other users from updating

--- protected/views/user/view.php
+++ protected/views/user/view.php
@@ -4,18 +4,7 @@ $this->breadcrumbs=array(
 	$model->fullName,
 );
 
-$this->menu=array(
-	array('label'=>'Update Profile', 
-		'url'=>array('update', 'id'=>$model->id),
-		'linkOptions'=>array('id'=>'user_update_menu_item'), 
-		'visible'=>Yii::app()->user->id == $model->id,
-	),
-	array('label'=>'Update Password', 
-		'url'=>array('updatepassword', 'id'=>$model->id),
-		'linkOptions'=>array('id'=>'user_update_menu_item'), 
-		'visible'=>Yii::app()->user->id == $model->id,
-	),
-);
+$this->menu=array();
 ?>
 
 <h1><?php echo $model->fullName; ?></h1>
