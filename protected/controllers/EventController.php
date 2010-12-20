d69c5cd34612b402d4a69cc2d0118c4d080205da
ajsharma <ajsharma@f94a5964-8f9d-401d-8ecb-23302dea6c3a>
2010-12-20 04:05:50 +0000

    Fixed bug where deleting an event took you to the admin page

--- protected/controllers/EventController.php
+++ protected/controllers/EventController.php
@@ -140,7 +140,7 @@ class EventController extends Controller
 
 			// if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
 			if(!isset($_GET['ajax']))
-				$this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('admin'));
+				$this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('event/index'));
 		}
 		else
 			throw new CHttpException(400,'Invalid request. Please do not repeat this request again.');
