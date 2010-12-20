7afbd73424a0e845c1d64e3acd1b6b29a1aad4bd
ajsharma <ajsharma@f94a5964-8f9d-401d-8ecb-23302dea6c3a>
2010-12-20 01:30:22 +0000

    Adding gecko5 layouts

--- themes/lizard/views/layouts/column2.php
+++ themes/lizard/views/layouts/column2.php
@@ -1,40 +1,6 @@
 <?php $this->beginContent('//layouts/main'); ?>
-<div class="container">
-	<?php
-	if(isset($this->menu) 
-		&& !empty($this->menu)
-		&& !Yii::app()->user->isGuest
-		):?>
-		<div id="sidebar">
-		<?php 
-		$this->widget('zii.widgets.CMenu', array(
-			'items'=>$this->menu,
-		));
-		?>
-		</div><!-- end of sidebar -->
-	<?php endif; ?>
-	
-	
-	<!-- flash notices -->
-		<?php if(Yii::app()->user->hasFlash('error')):?>
-		    <div class="flash-error">
-		        <?php echo Yii::app()->user->getFlash('error'); ?>
-		    </div>
-		<?php endif; ?>
-		<?php if(Yii::app()->user->hasFlash('notice')):?>
-		    <div class="flash-notice">
-		        <?php echo Yii::app()->user->getFlash('notice'); ?>
-		    </div>
-		<?php endif; ?>
-		<?php if(Yii::app()->user->hasFlash('success')):?>
-		    <div class="flash-success">
-		        <?php echo Yii::app()->user->getFlash('success'); ?>
-		    </div>
-		<?php endif; ?>
-	
-	<div id="content">
-		<h1><?php echo $this->pageTitle; ?></h1>
-		<?php echo $content; ?>
-	</div><!-- end of content -->
-</div>
+<div id="content">
+	<h1><?php echo $this->pageTitle; ?></h1>
+	<?php echo $content; ?>
+</div><!-- end of content -->
 <?php $this->endContent(); ?>
--- themes/lizard/views/layouts/main.php
+++ themes/lizard/views/layouts/main.php
@@ -1,8 +1,7 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<!doctype html> 
+<html>
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<meta name="language" content="en" />
+	<meta charset="utf-8">
 
 	<!-- blueprint CSS framework -->
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->theme->baseUrl; ?>/css/reset.css" />
@@ -16,36 +15,67 @@
 	<link rel="shortcut icon" href="<?php echo Yii::app()->request->baseUrl; ?>/images/favicon.ico"/> 
 
 	<title><?php echo CHtml::encode($this->pageTitle) . ' - ' . Yii::app()->name; ?></title>
+	<?php Yii::app()->clientScript->registerScriptFile(Yii::app()->getBaseUrl() . '/js/libs/modernizr-1.6.min.js'); ?>	
 </head>
-
 <body>
-
-	<div class="bodycontainer" id="page">
+<div class="bodycontainer" id="page">
+<div id="mainnav">
+<?php $this->widget('zii.widgets.CMenu', array(
+	'items'=>array(
+		array('label'=>'Home', 'url'=>array('/site/index')),
+		array('label'=>'Groups', 'url'=>array('/group/index'), 'visible'=>!Yii::app()->user->isGuest),
+		array('label'=>'Events', 'url'=>array('/event/index'), 'visible'=>!Yii::app()->user->isGuest),
+		array('label'=>'Settings', 'url'=>array('/site/settings'), 'visible'=>!Yii::app()->user->isGuest),
+		array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
+		array('label'=>'Logout ('.Yii::app()->user->model->firstName.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest),
+		array('label'=>'Admin', 'url'=>array('/user/admin'), 'visible'=>Yii::app()->user->isAdmin),
+	),
+)); 
+?>
+</div><!-- mainnav -->
+		
+	<?php
+	if(isset($this->menu) 
+		&& !empty($this->menu)
+		&& !Yii::app()->user->isGuest
+	):?>
+		<div id="subnav">
+		<?php 
+		$this->widget('zii.widgets.CMenu', array(
+			'items'=>$this->menu,
+		));
+		?>
+		</div><!-- end of subnav -->
+	<?php endif; ?>
 	
-		<div id="mainmenu">
-			<?php $this->widget('zii.widgets.CMenu', array(
-				'items'=>array(
-					//array('label'=>'Welcome', 'url'=>array('/site/page', 'view'=>'Welcome')),		
-					array('label'=>'Home', 'url'=>array('/site/index')),
-					array('label'=>'Groups', 'url'=>array('/group/index'), 'visible'=>!Yii::app()->user->isGuest),
-					array('label'=>'Events', 'url'=>array('/event/index'), 'visible'=>!Yii::app()->user->isGuest),
-					array('label'=>'Settings', 'url'=>array('/site/settings'), 'visible'=>!Yii::app()->user->isGuest),
-					array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
-					array('label'=>'Logout ('.Yii::app()->user->model->firstName.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest),
-					array('label'=>'Admin', 'url'=>array('/user/admin'), 'visible'=>Yii::app()->user->isAdmin),
-				),
-			)); 
-			?>
-		</div><!-- mainmenu -->
-
+	
+	<!-- flash notices -->
+		<?php if(Yii::app()->user->hasFlash('error')):?>
+		    <div class="flash-error">
+		        <?php echo Yii::app()->user->getFlash('error'); ?>
+		    </div>
+		<?php endif; ?>
+		<?php if(Yii::app()->user->hasFlash('notice')):?>
+		    <div class="flash-notice">
+		        <?php echo Yii::app()->user->getFlash('notice'); ?>
+		    </div>
+		<?php endif; ?>
+		<?php if(Yii::app()->user->hasFlash('success')):?>
+		    <div class="flash-success">
+		        <?php echo Yii::app()->user->getFlash('success'); ?>
+		    </div>
+		<?php endif; ?>
+	</header>
+	
+	<article>
 		<?php echo $content; ?>
+	</article>
 	
-	</div><!-- page -->
+</div><!-- page -->
 	
-	<div id="footer">
-			Poncla &copy; <?php echo date('Y'); ?><br/>
-			All Rights Reserved.<br/>
-	</div><!-- footer -->
+<footer id="footer">
+	<span>Poncla &copy; <?php echo date('Y'); ?> All Rights Reserved.</span>
+</footer><!-- footer -->
 
 </body>
 </html>
