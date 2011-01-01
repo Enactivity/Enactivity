a1af16d0d3ddf4041ad64d793dbfcf124b8efc62
ajsharma <ajsharma@f94a5964-8f9d-401d-8ecb-23302dea6c3a>
2011-01-01 19:17:38 +0000

    Removed ContactForm

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -59,26 +59,6 @@ class SiteController extends Controller
 	}
 
 	/**
-	 * Displays the contact page
-	 */
-	public function actionContact()
-	{
-		$model = new ContactForm;
-		if(isset($_POST['ContactForm']))
-		{
-			$model->attributes=$_POST['ContactForm'];
-			if($model->validate())
-			{
-				$headers="From: {$model->email}\r\nReply-To: {$model->email}";
-				mail(Yii::app()->params['adminEmail'],$model->subject,$model->body,$headers);
-				Yii::app()->user->setFlash('success','Thank you for contacting us. We will respond to you as soon as possible.');
-				$this->refresh();
-			}
-		}
-		$this->render('contact',array('model'=>$model));
-	}
-
-	/**
 	 * Displays the login page
 	 */
 	public function actionLogin()
