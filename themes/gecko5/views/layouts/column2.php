1eb60761005aec66352ed5febfcb2129ee18330e
ajsharma <ajsharma@f94a5964-8f9d-401d-8ecb-23302dea6c3a>
2011-01-07 01:08:27 +0000

    Added encoding to pageTitles

--- themes/gecko5/views/layouts/column2.php
+++ themes/gecko5/views/layouts/column2.php
@@ -1,7 +1,7 @@
 <?php $this->beginContent('//layouts/main'); ?>
 <article id="content">
 	<header>
-		<h1><?php echo $this->pageTitle; ?></h1>
+		<h1><?php echo CHtml::encode($this->pageTitle); ?></h1>
 	</header>
 	<?php echo $content; ?>
 </article><!-- end of content -->
