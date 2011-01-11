bba60a35d9826e70be07922dbac604ec925ecadd
ajsharma <ajsharma@f94a5964-8f9d-401d-8ecb-23302dea6c3a>
2011-01-11 07:17:28 +0000

    removing old theme

--- themes/lizard/views/layouts/column2.php
+++
@@ -1,6 +0,0 @@
-<?php $this->beginContent('//layouts/main'); ?>
-<div id="content">
-	<h1><?php echo $this->pageTitle; ?></h1>
-	<?php echo $content; ?>
-</div><!-- end of content -->
-<?php $this->endContent(); ?>
