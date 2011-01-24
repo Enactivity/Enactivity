167fd7fe877aece3754b4d57a2a00b37d98193e5
ajsharma <ajsharma@f94a5964-8f9d-401d-8ecb-23302dea6c3a>
2011-01-24 00:14:47 +0000

    Adding UserLink widget

--- protected/views/groupbanter/_view.php
+++ protected/views/groupbanter/_view.php
@@ -1,23 +1,25 @@
 <div class="view">
-	<h2><?php 
-		echo CHtml::link(StringUtils::truncate(CHtml::encode($data->content), 80), 
+<dl>
+	<dt><h2><?php 
+		echo PHtml::link(StringUtils::truncate(PHtml::encode($data->content), 80), 
 			array('groupbanter/view', 'id'=>$data->id)); 
-	?></h2>
+	?></h2></dt>
 	
-	<b><?php echo CHtml::encode($data->getAttributeLabel('creatorId')); ?>:</b>
-	<?php echo CHtml::encode($data->creator->fullName); ?>
-	<br />
+	<dd><b><?php echo PHtml::encode($data->getAttributeLabel('creatorId')); ?>:</b>
+	<?php $this->widget('ext.widgets.UserLink', array(
+		'userModel' => $data->creator,
+	)); 
+	?></dd>
 
-	<b><?php echo CHtml::encode($data->getAttributeLabel('created')); ?>:</b>
-	<?php echo CHtml::encode(Yii::app()->dateformatter->formatDateTime($data->created, 
-		'full', 'short')); ?>
-	<br />
+	<dd><b><?php echo PHtml::encode($data->getAttributeLabel('created')); ?>:</b>
+	<?php echo PHtml::encode(Yii::app()->dateformatter->formatDateTime($data->created, 
+		'full', 'short')); ?></dd>
 	
 	<?php if($model->modified != $model->created): ?>
-	<b><?php echo CHtml::encode($data->getAttributeLabel('modified')); ?>:</b>
-	<?php echo CHtml::encode(Yii::app()->dateformatter->formatDateTime($data->modified, 
-		'full', 'short')); ?>
-	<br />
+	<dd><b><?php echo PHtml::encode($data->getAttributeLabel('modified')); ?>:</b>
+	<?php echo PHtml::encode(Yii::app()->dateformatter->formatDateTime($data->modified, 
+		'full', 'short')); ?></dd>
 	<?php endif; ?>
-
+	
+</dl>
 </div>
