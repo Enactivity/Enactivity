875aea499ef7c37eced054a93ea6aa492de9ba6e
ajsharma <ajsharma@f94a5964-8f9d-401d-8ecb-23302dea6c3a>
2011-01-26 07:23:16 +0000

    Cleaned up eventbanter update view to make it look similar to groupbanter update view

--- protected/views/eventbanter/update.php
+++ protected/views/eventbanter/update.php
@@ -1,13 +1,5 @@
 <?php
-
-$this->menu=array(
-	array('label'=>'List EventBanter', 'url'=>array('index')),
-	array('label'=>'Create EventBanter', 'url'=>array('create')),
-	array('label'=>'View EventBanter', 'url'=>array('view', 'id'=>$model->id)),
-	array('label'=>'Manage EventBanter', 'url'=>array('admin')),
-);
+$this->pageTitle = StringUtils::truncate($model->content, 60);
 ?>
-
-<h1>Update EventBanter <?php echo $model->id; ?></h1>
 
 <?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
