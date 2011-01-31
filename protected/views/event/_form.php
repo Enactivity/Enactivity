44a8a32a635da3cf09d22e7c1becc93ebc0fa29e
ajsharma <ajsharma@f94a5964-8f9d-401d-8ecb-23302dea6c3a>
2011-01-31 00:40:19 +0000

    Removing out of date FIXME

--- protected/views/event/_form.php
+++ protected/views/event/_form.php
@@ -21,7 +21,6 @@
 		<div class="formlabel"><?php echo $form->labelEx($model,'starts'); ?></div>
 		<div class="forminput"><?php
 			// preformat date before loading into widget 
-			// FIXME: this doesn't link properly
 			$model->starts = isset($model->starts) ? date('m/d/Y g:i a', strtotime($model->starts)) : null;
 			$this->widget('ext.widgets.jui.JuiDateTimePicker', 
 				array(
