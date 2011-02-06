ce76b93005c7f7114843100616b8f7800a7144cc
hvuong <hvuong@f94a5964-8f9d-401d-8ecb-23302dea6c3a>
2011-02-06 23:39:09 +0000

    - adding buffer days to calendar

--- protected/views/event/calendar.php
+++ protected/views/event/calendar.php
@@ -21,26 +21,50 @@ $this->pageMenu[] = array(
 ?>
 
 <?php
+$daytitle = array ("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+foreach ($daytitle as $title) {
+		echo PHtml::tag('dl', array('class' => 'daytitle'));
+		echo PHtml::tag('dt');
+		echo $title;
+		echo PHtml::closetag('dt');
+		echo PHtml::closetag('dl');
+}
+?>
+
+<?php
 $numDays = cal_days_in_month(CAL_GREGORIAN, $month, $year);
 $events = $dataProvider->getData();
 
-for($day = 1; $day <= $numDays; $day++) {
+$firstDayofMonth = getdate(mktime(0, 0, 0, $month, 1, $year));
+$dayoftheweek = $firstDayofMonth["wday"];
+$i = 1 - $dayoftheweek;
+$lastDayOfMonth = getdate(mktime(0, 0, 0, $month, $numDays, $year));
+$lastdayoftheweekofthemonth = $lastDayOfMonth["wday"];
+$bufferDays = $numDays + 6 - $lastdayoftheweekofthemonth;	
+
+for($day = $i; $day <= $bufferDays; $day++) {
 	$currentDayStart = mktime(0, 0, 0, $month, $day, $year);
 	$currentDayEnd = mktime(23, 59, 59, $month, $day, $year);
+	//add styles to day based on this or last month
+	$htmlOptions = array();
+	$htmlOptions['class'] = 'day';
+	if(($day < 1) || ($day > $numDays)) {
+		$htmlOptions['class'] .= ' notThisMonth';
+	}
 	
-	echo PHtml::tag('dl', array('class' => 'day'));
-	echo PHtml::tag('dd');
+	echo PHtml::tag('dl', $htmlOptions);
+	echo PHtml::tag('dt');
 	echo date('d', mktime(0, 0, 0, $month, $day, $year));
-	echo PHtml::closeTag('dd');
+	echo PHtml::closeTag('dt');
 	
 	foreach($events as $event) {
 		if((Date::MySQLDateOffset($event->starts) <= $currentDayEnd)
 		&& (Date::MySQLDateOffset($event->ends) >= $currentDayStart)) {
-			echo PHtml::tag('dt');
+			echo PHtml::tag('dd');
 			echo PHtml::link(PHtml::encode($event->startTime) . " " . PHtml::encode($event->name), 
 				array('view', 'id'=>$event->id)
 				); 
-			echo PHtml::closeTag('dt');
+			echo PHtml::closeTag('dd');
 		}
 	}
 	
