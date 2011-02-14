d4c3c1248327c8871e7d897bb117c508e0a304e6
ajsharma <ajsharma@f94a5964-8f9d-401d-8ecb-23302dea6c3a>
2011-02-14 03:28:52 +0000

    Wrote in a hack to make feed index _view display name/descriptions

--- protected/views/feed/_view.php
+++ protected/views/feed/_view.php
@@ -1,10 +1,27 @@
 <article class="view">
 <dl>
-	<dt><?php //on create ?>
+	<dt>
+	<?php 
+	// hack to make feed names clearer
+	$description = '';
+	if(isset($data->modelObject->name)) {
+		$description = $data->modelObject->name;
+	}
+	elseif(isset($data->modelObject->description)) {
+		$description = $data->modelObject->description;
+	}
+	elseif(isset($data->modelObject->content)) {
+		$description = $data->modelObject->content;
+	}
+	else {
+		$description = $data->model;
+	}
+	
+	?>
 	<?php $this->widget('ext.widgets.UserLink', array(
 		'userModel' => $data->user,
 	)); ?> <?php echo strtolower($data->action); ?>d 
-	<?php echo CHtml::link(CHtml::encode($data->model), array(strtolower($data->model) . '/view', 'id'=>$data->modelId)); ?>
+	<?php echo CHtml::link(CHtml::encode($description), array(strtolower($data->model) . '/view', 'id'=>$data->modelId)); ?>
 	</dt>
 	<dd><span><?php echo PHtml::encode(Yii::app()->dateformatter->formatDateTime($data->created, 
 		'full', 'short')); ?></span></dd>
