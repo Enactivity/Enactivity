8bf096a1ef7f59d3a0c8cf92e5040d3e89fe4fa4
ajsharma <ajsharma@f94a5964-8f9d-401d-8ecb-23302dea6c3a>
2011-02-14 03:04:17 +0000

    Fixing feed links to be lowercase for unix environments

--- protected/views/feed/_view.php
+++ protected/views/feed/_view.php
@@ -4,7 +4,7 @@
 	<?php $this->widget('ext.widgets.UserLink', array(
 		'userModel' => $data->user,
 	)); ?> <?php echo strtolower($data->action); ?>d 
-	<?php echo CHtml::link(CHtml::encode($data->model), array($data->model . '/view', 'id'=>$data->modelId)); ?>
+	<?php echo CHtml::link(CHtml::encode($data->model), array(strtolower($data->model) . '/view', 'id'=>$data->modelId)); ?>
 	</dt>
 	<dd><span><?php echo PHtml::encode(Yii::app()->dateformatter->formatDateTime($data->created, 
 		'full', 'short')); ?></span></dd>
