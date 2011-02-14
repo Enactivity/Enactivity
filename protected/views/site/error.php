e83009f3b1c8308d69d5577d79d66c756e574c0a
ajsharma <ajsharma@f94a5964-8f9d-401d-8ecb-23302dea6c3a>
2011-02-14 01:55:14 +0000

    Updated error page message

--- protected/views/site/error.php
+++ protected/views/site/error.php
@@ -3,5 +3,5 @@ $this->pageTitle = 'What did you do?!';
 ?>
 
 <div class="error"><p><?php 
-	echo PHtml::encode("Don't worry, the Poncla team's been notified."); 
+	echo PHtml::encode("Don't worry, we're just messing with ya.  But it does look like you've run into an unexpected error.  The Poncla team's been notified and will work hard to make sure it doesn't happen again."); 
 ?></p></div>
