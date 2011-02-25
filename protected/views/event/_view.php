1a79ee5e77d48b82a01b5317fd7e9e1cbd2a9e17
ajsharma <ajsharma@f94a5964-8f9d-401d-8ecb-23302dea6c3a>
2011-02-25 05:55:17 +0000

    Re-wrote event _view to be full php

--- protected/views/event/_view.php
+++ protected/views/event/_view.php
@@ -1,21 +1,60 @@
-<article class="view">
-<dl>
-	<dt><h1><?php 
-		echo PHtml::link(PHtml::encode($data->name), 
-			array('view', 'id'=>$data->id)); 
-	?></h1></dt>
-	<dd><span class="eventdate"><?php echo PHtml::encode($data->datesAsSentence); ?></span></dd>
-	<dd><span class="attendeecount"><?php echo $data->eventUsersAttendingCount == 1 
-		? $data->eventUsersAttendingCount . " person attending" 
-		: $data->eventUsersAttendingCount . " people attending";?></span></dd>
-</dl>
-	<footer>
-	<?php 
-	//RSVP buttons
-	$this->renderPartial('_rsvp', array(
-		'event'=>$data,
-		'eventuser'=>$data->getRSVP(Yii::app()->user->id),
-	)); 
-	?>
-	</footer>
-</article>
+<?php 
+// TODO: PHPDocs file info
+
+// start article
+echo PHtml::openTag('article', array(
+	'class' => 'view',		
+));
+
+// start headers
+echo PHtml::openTag('header');
+echo PHtml::openTag('hgroup');
+
+// event name
+echo PHtml::openTag('h1');
+echo PHtml::link(PHtml::encode($data->name), 
+	array('view', 'id'=>$data->id)
+); 
+echo PHtml::closeTag('h1');
+
+// event date
+echo PHtml::openTag('h2');
+echo PHtml::openTag('time');
+echo PHtml::encode($data->datesAsSentence);
+echo PHtml::closeTag('time');
+echo PHtml::closeTag('h2');
+
+// event attendee count
+echo PHtml::openTag('h2');
+echo PHtml::openTag('span');
+echo $data->eventUsersAttendingCount;
+echo $data->eventUsersAttendingCount == 1 
+	? ' person attending' 
+	: ' people attending';
+echo PHtml::closeTag('span');
+echo PHtml::closeTag('h2');
+
+// close headers
+echo PHtml::closeTag('hgroup');
+echo PHtml::closeTag('header');
+
+// body
+echo StringUtils::truncate(
+	Yii::app()->format->formatStyledText($data->description),
+	250);
+// end body
+
+// start footer
+echo PHtml::openTag('footer');
+
+// RSVP menu
+echo PHtml::openTag('menu');
+$this->renderPartial('_rsvp', array(
+	'event'=>$data,
+	'eventuser'=>$data->getRSVP(Yii::app()->user->id),
+)); 
+echo PHtml::closeTag('menu');
+echo PHtml::closeTag('footer');
+
+// close article
+echo PHtml::closeTag('article');
