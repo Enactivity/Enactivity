f47e9327f0a49ab271e7ab830125b73f61ea8e05
ajsharma <ajsharma@f94a5964-8f9d-401d-8ecb-23302dea6c3a>
2011-02-26 06:14:22 +0000

    moved tertiary nav to content header

--- protected/views/layouts/defaultlayout.php
+++ protected/views/layouts/defaultlayout.php
@@ -2,20 +2,20 @@
 <section id="content" class="<?php echo $this->id . " " . $this->action->id;?>">
 	<header>
 		<h1><?php echo CHtml::encode($this->pageTitle); ?></h1>
-	</header>
 	
-<?php if(isset($this->pageMenu) 
-	&& !empty($this->pageMenu)
-	&& !Yii::app()->user->isGuest
-):?>
-	<nav id="tertiaryNavigation">
-<?php 
-$this->widget('zii.widgets.CMenu', array(
-	'items'=>$this->pageMenu,
-));
-?>
-	</nav><!-- end of tertiaryNavigation -->
-<?php endif; ?>
+		<?php if(isset($this->pageMenu) 
+			&& !empty($this->pageMenu)
+			&& !Yii::app()->user->isGuest
+		):?>
+		<nav id="tertiaryNavigation">
+		<?php 
+		$this->widget('zii.widgets.CMenu', array(
+			'items'=>$this->pageMenu,
+		));
+		?>
+		</nav><!-- end of tertiaryNavigation -->
+	<?php endif; ?>
+	</header>
 	
 	<div id="contentBody">
 	<?php echo $content; ?>
