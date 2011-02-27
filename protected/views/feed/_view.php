767a815fc4881a3b07bece779742fe67920daa99
ajsharma <ajsharma@f94a5964-8f9d-401d-8ecb-23302dea6c3a>
2011-02-27 20:58:09 +0000

    Re-wrote feed _view to be php script

--- protected/views/feed/_view.php
+++ protected/views/feed/_view.php
@@ -1,16 +1,33 @@
-<article class="view">
-<dl>
-	<dt>
-	<?php 
-	$description = isset($data->modelObject) ? $data->modelObject->feedAttribute : $data->model;
+<?php 
+// start article
+echo PHtml::openTag('article', array(
+	'class' => 'view'
+));
 
-	?>
-	<?php $this->widget('ext.widgets.UserLink', array(
-		'userModel' => $data->user,
-	)); ?> <?php echo strtolower($data->action); ?>
-	<?php echo CHtml::link(StringUtils::truncate(PHtml::encode($description), 80), array(strtolower($data->model) . '/view', 'id'=>$data->modelId)); ?>
-	</dt>
-	<dd><span><?php echo PHtml::encode(Yii::app()->dateformatter->formatDateTime($data->created, 
-		'full', 'short')); ?></span></dd>
-</dl>
-</article>
+// start definition list
+echo PHtml::openTag('dl');
+echo PHtml::openTag('dt');
+
+$this->widget('ext.widgets.UserLink', array(
+	'userModel' => $data->user,
+)); 
+echo ' ';
+echo strtolower($data->action);
+echo ' '; 
+$description = isset($data->modelObject) ? $data->modelObject->feedAttribute : $data->model;
+echo CHtml::link(StringUtils::truncate(PHtml::encode($description), 80), array(strtolower($data->model) . '/view', 'id'=>$data->modelId));
+
+echo PHtml::closeTag('dt');
+
+echo PHtml::openTag('dd');
+echo PHtml::openTag('span');
+
+echo PHtml::encode(
+	Yii::app()->dateformatter->formatDateTime($data->created, 'full', 'short')
+);
+
+echo PHtml::closeTag('span');
+echo PHtml::closeTag('dd');
+
+echo PHtml::closeTag('dl');
+echo PHtml::closeTag('article');
