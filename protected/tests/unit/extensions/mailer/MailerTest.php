173ec4d29f93b007fb88add4db40a42c45668472
lhuynh <lhuynh@f94a5964-8f9d-401d-8ecb-23302dea6c3a>
2011-03-07 05:06:00 +0000

    Added Unit testing for Mailer extension

---
+++ protected/tests/unit/extensions/mailer/MailerTest.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Unit test for Mailer extension, Mailer parent class
+ * PasswordEmail arbitrarily chosen as the type of Mailer to test send()
+ * @author Long Huynh
+ */
+
+Yii::import('application.extensions.mailer.Mailer');
+Yii::import('application.extensions.mailer.PasswordEmail');
+
+class MailerTest extends PHPUnit_Framework_TestCase {
+	
+	public function testSendIsCalled() {	
+		$to = StringUtils::createRandomString(30);
+		$newpassword = StringUtils::createRandomString(10);
+		
+		$childClass = $this->getMock('PasswordEmail');
+		$childClass->to = $to;
+		$childClass->newpassword = $newpassword;
+		$childClass->expects($this->once())
+					->method(send);
+					
+		$testClass = new Mailer;
+		$testClass->send($childClass);
+	}
+	
+	public function testTransmit() {
+		$to = StringUtils::createRandomString(30);
+		$newPassword = StringUtils::createRandomString(10);
+		$sendTest = false;
+		
+		$testClass = new PasswordEmail;
+		$testClass->to = $to;
+		$testClass->newpassword = $newPassword;
+		$testClass->sendTest = $sendTest;
+
+		//Commented out since this will generate an error
+		//when a mail transport has not be installed
+		//$testClass->send();
+	}
+}
