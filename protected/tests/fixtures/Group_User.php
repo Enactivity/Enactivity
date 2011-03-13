cacc778e317da36d2a34c8e49f59f54dd0e3d87f
ajsharma <ajsharma@f94a5964-8f9d-401d-8ecb-23302dea6c3a>
2011-03-13 19:52:44 +0000

    Adding group_user fixture

---
+++ protected/tests/fixtures/Group_User.php
@@ -0,0 +1,25 @@
+<?php 
+/**
+ * Fixture class for GroupUser table
+ */
+
+// get the groups and users
+$groups = $this->getRows('groups');
+$users = $this->getRows('users');
+
+return array(
+	'testGroupAdminUser' => array(
+		'groupId' => 1,
+		'userId' => 1,
+		'status' => User::STATUS_ACTIVE,
+		'created' => date ("Y-m-d H:i:s", strtotime("-1 hours")),
+		'modified' => date ("Y-m-d H:i:s", strtotime("-1 hours")),
+	),
+	'testGroupRegisteredUser' => array(
+		'groupId' => 2,
+		'userId' => 2,
+		'status' => User::STATUS_ACTIVE,
+		'created' => date ("Y-m-d H:i:s", strtotime("-1 hours")),
+		'modified' => date ("Y-m-d H:i:s", strtotime("-1 hours")),
+	),
+);
