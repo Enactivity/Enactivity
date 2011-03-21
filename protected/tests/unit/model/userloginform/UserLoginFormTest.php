92f4d73104e7480408d885f640584f0549400b2e
afong <afong@f94a5964-8f9d-401d-8ecb-23302dea6c3a>
2011-03-21 04:09:52 +0000

    More tests for UserLoginForm

--- protected/tests/unit/model/userloginform/UserLoginFormTest.php
+++ protected/tests/unit/model/userloginform/UserLoginFormTest.php
@@ -40,28 +40,27 @@ class UserLoginFormTest extends DbTestCase
 	    ));
 	    $this->assertFalse($formUnderTest->validate(), 'empty password was marked as valid');
 	}
-	
-/*	public function testAuthenticateValid() {
-		$email = $this->users['registered']['email'];
-		$password = $this->users['registered']['password'];
-		$formUnderTest = new UserLoginForm();
-		$formUnderTest->setAttributes(array(
-	        'email' => $email,
-			'password' => $password,
-	    ));
-	    $this->assertTrue($formUnderTest->validate(), 'valid user was marked as invalid');
-	}
 
+	//TODO: Password should not be hard coded	
 	public function testLoginValid() {
-		$email = $this->users['registered']['email'];
-		$password = $this->users['registered']['password'];
+		$email = $this->users['admin']['email'];
+		$password = 'test';
 		$formUnderTest = new UserLoginForm();
 		$formUnderTest->setAttributes(array(
 	        'email' => $email,
 			'password' => $password,
 	    ));
-	    var_dump($formUnderTest->login());	
+	    $this->assertTrue($formUnderTest->login(), 'valid user was unable to log in');
 	}
-*/	
 
+	public function testLoginInvalid() {
+		$email = $this->users['admin']['email'];
+		$password = StringUtils::createRandomString(30);
+		$formUnderTest = new UserLoginForm();
+		$formUnderTest->setAttributes(array(
+	        'email' => $email,
+			'password' => $password,
+	    ));
+	    $this->assertFalse($formUnderTest->login(), 'invalid user was able to log in');
+	}
 }
