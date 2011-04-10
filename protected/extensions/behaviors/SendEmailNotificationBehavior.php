870f60ef3f5304a863607097c6afe62b0527bca3
ajsharma <ajsharma@f94a5964-8f9d-401d-8ecb-23302dea6c3a>
2011-04-10 00:44:51 +0000

    Initial submission of email notification behavior

---
+++ protected/extensions/behaviors/SendEmailNotificationBehavior.php
@@ -0,0 +1,102 @@
+<?php 
+
+/**
+ * This is the behavior class for behavior "SendEmailNotificationBehavior".
+ *
+ * Models wishing to use this behavior must have a public $groupId int value
+ */
+class SendEmailNotificationBehavior extends CActiveRecordBehavior
+{
+	/**
+	 * List of attributes that should be ignored by the log
+	 * when the ActiveRecord is updated.
+	 * @var array
+	 */
+	public $ignoreAttributes = array();
+	
+	/**
+	 * The attribute that the feed should use to identify the model
+	 * to the user
+	 * @var string
+	 */
+	public $feedAttribute = '';
+	
+	private $_oldAttributes = array();
+ 
+	public function afterSave($event) {
+		if (!$this->Owner->isNewRecord) {
+ 
+			// new attributes
+			$newAttributes = $this->Owner->getAttributes();
+			$oldAttributes = $this->getOldAttributes();
+ 
+			// compare old and new
+			foreach ($newAttributes as $name => $value) {
+				if(!in_array($name, $this->ignoreAttributes)) {
+					if (!empty($oldAttributes)) {
+						$old = $oldAttributes[$name];
+					} else {
+						$old = '';
+					}
+	 
+					if ($value != $old) {
+						//$changes = $name . ' ('.$old.') => ('.$value.'), ';
+	 
+						$log = new ActiveRecordLog;
+	//					$log->description=  'User ' . Yii::app()->user->Name 
+	//											. ' changed ' . $name . ' for ' 
+	//											. get_class($this->Owner) 
+	//											. '[' . $this->Owner->getPrimaryKey() .'].';
+						$log->groupId = $this->Owner->groupId;
+						$log->action = 'updated';
+						$log->model = get_class($this->Owner);
+						$log->modelId = $this->Owner->getPrimaryKey();
+						$log->modelAttribute = $name;
+						$log->userId = Yii::app()->user->id;
+						$log->save();
+					}
+				}
+			}
+		} 
+		else {
+			$log = new ActiveRecordLog;
+//			$log->description=  'User ' . Yii::app()->user->Name 
+//									. ' created ' . get_class($this->Owner) 
+//									. '[' . $this->Owner->getPrimaryKey() .'].';
+			$log->groupId = $this->Owner->groupId;
+			$log->action = 'posted';
+			$log->model = get_class($this->Owner);
+			$log->modelId = $this->Owner->getPrimaryKey();
+			$log->modelAttribute = '';
+			$log->userId = Yii::app()->user->id;
+			$log->save();
+		}
+	}
+ 
+	public function afterDelete($event) {
+		$log = new ActiveRecordLog;
+//		$log->description =  'User ' . Yii::app()->user->Name . ' deleted ' 
+//								. get_class($this->Owner) 
+//								. '[' . $this->Owner->getPrimaryKey() .'].';
+		$log->groupId = $this->Owner->groupId;
+		$log->action = 'deleted';
+		$log->model = get_class($this->Owner);
+		$log->modelId = $this->Owner->getPrimaryKey();
+		$log->modelAttribute = '';
+		$log->userId = Yii::app()->user->id;
+		$log->save();
+	}
+ 
+	public function afterFind($event) {
+		// Save old values
+		$this->setOldAttributes($this->Owner->getAttributes());
+	}
+ 
+	public function getOldAttributes() {
+		return $this->_oldAttributes;
+	}
+ 
+	public function setOldAttributes($value) {
+		$this->_oldAttributes = $value;
+	}
+}
