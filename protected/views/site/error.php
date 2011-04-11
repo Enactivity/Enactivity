b1809a045f058fa2033a7bedd9e2532b01571f57
afong <afong@f94a5964-8f9d-401d-8ecb-23302dea6c3a>
2011-04-11 01:41:10 +0000

    Updating error page to be more professional

--- protected/views/site/error.php
+++ protected/views/site/error.php
@@ -1,7 +1,7 @@
 <?php
-$this->pageTitle = 'What did you do?!';
+$this->pageTitle = 'Our apologies';
 ?>
 
 <div class="error"><p><?php 
-	echo PHtml::encode("Don't worry, we're just messing with ya.  But it does look like you've run into an unexpected error.  The Poncla team's been notified and will work hard to make sure it doesn't happen again."); 
+	echo PHtml::encode("Sorry, you've run into an unexpected error. The Poncla team's been notified and will work hard to make sure it doesn't happen again."); 
 ?></p></div>
