b3dbec015c6f101de81ad2eb8bd94fd52808ebaf
hvuong <hvuong@f94a5964-8f9d-401d-8ecb-23302dea6c3a>
2011-04-18 01:19:43 +0000

    - removed goal input box in view - removed unnecessary information displaying in goal view

--- protected/views/goal/view.php
+++ protected/views/goal/view.php
@@ -4,20 +4,10 @@
 	
 	$this->pageMenu = MenuDefinitions::goalMenu($model);
 ?>
-<!-- 
 <p>
-	You may change the content of this page by modifying
-	the file <tt><?php echo __FILE__; ?></tt>.
-</p>
--->
-
 <?php 
-// "what would you want to do input" box
-echo $this->renderPartial('_form', array('model'=>new Goal(Goal::SCENARIO_INSERT)));
-?>
-
-<p>
-<?php $this->widget('zii.widgets.CDetailView', array(
+/**
+	$this->widget('zii.widgets.CDetailView', array(
 	'data'=>$model,
 	'attributes'=>array(
 		'id',
@@ -29,7 +19,7 @@ echo $this->renderPartial('_form', array('model'=>new Goal(Goal::SCENARIO_INSERT
 		'created',
 		'modified',
 	),
-)); ?>
+)); **/?>
 </p>
 
 <?php $this->widget('zii.widgets.CListView', array(
