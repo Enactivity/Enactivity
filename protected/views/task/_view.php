b07a7634d6adcc66dc5d8e7f81d940f6ea9a89f1
hvuong <hvuong@f94a5964-8f9d-401d-8ecb-23302dea6c3a>
2011-04-18 02:08:59 +0000

    - fixed link to show /task/view/id

--- protected/views/task/_view.php
+++ protected/views/task/_view.php
@@ -1,6 +1,6 @@
 <div class="view">
 	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
-	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)); ?>
+	<?php echo CHtml::link(CHtml::encode($data->id), array('/task/view', 'id'=>$data->id)); ?>
 	<br />
 
 	<b><?php echo CHtml::encode($data->getAttributeLabel('goalId')); ?>:</b>
