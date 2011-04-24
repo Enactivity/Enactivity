6eeb9774f177bc041a47967c13eb6ea4bef37f88
ajsharma <ajsharma@f94a5964-8f9d-401d-8ecb-23302dea6c3a>
2011-04-24 21:42:07 +0000

    Adding goal fixtures

---
+++ protected/tests/fixtures/goal.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Fixture class for Goal table
+ */
+
+$users = $this->getRows('User');
+$groups = $this->getRows('Group');
+
+return array(
+	'testgoal' => array(
+		'name' => 'test goal',
+		'groupId' => 1,
+		'ownerId' => null,
+		'isCompleted' => '0',
+		'isTrash' => '0',
+		'created' => date ("Y-m-d H:i:s", strtotime("-1 hours")),
+		'modified' => date ("Y-m-d H:i:s", strtotime("-1 hours")),
+	),
+);
