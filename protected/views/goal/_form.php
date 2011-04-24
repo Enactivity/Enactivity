6463f0febe1bbc577ce831356f1abb70b660b325
ajsharma <ajsharma@f94a5964-8f9d-401d-8ecb-23302dea6c3a>
2011-04-24 22:09:30 +0000

    -Removed div from goal form -Set widget to ext.widgets.ActiveForm

--- protected/views/goal/_form.php
+++ protected/views/goal/_form.php
@@ -1,11 +1,7 @@
-<div class="form">
-
-<?php $form=$this->beginWidget('CActiveForm', array(
+<?php $form=$this->beginWidget('ext.widgets.ActiveForm', array(
 	'id'=>'goal-form',
 	'enableAjaxValidation'=>false,
 )); ?>
-
-	<p class="note">Fields with <span class="required">*</span> are required.</p>
 
 	<?php echo $form->errorSummary($model); ?>
 	<?php 
@@ -50,5 +46,3 @@
 	</div>
 
 <?php $this->endWidget(); ?>
-
-</div><!-- form -->
