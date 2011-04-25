a9b58bf572f02eb85f0ead90576bcf94fe6ca469
ajsharma <ajsharma@f94a5964-8f9d-401d-8ecb-23302dea6c3a>
2011-04-25 06:41:11 +0000

    Temporary clean up of task _view

--- protected/views/task/_view.php
+++ protected/views/task/_view.php
@@ -1,48 +1,64 @@
-<div class="view">
+<?php 
+/**
+ * View for individual task models
+ * 
+ * @param Task $data model
+ */
 
-	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
-	<?php echo CHtml::link(CHtml::encode($data->id), array('/task/view', 'id'=>$data->id)); ?>
-	<br />
+// calculate article class
+$articleClass = "view";
+$articleClass .= " task";
+$articleClass .= " task-" . $data->id;
+$articleClass .= $data->isCompleted ? " completed" : " not-completed";
+$articleClass .= $data->isTrash ? " trash" : " not-trash";
 
-	<b><?php echo CHtml::encode($data->getAttributeLabel('goalId')); ?>:</b>
-	<?php echo CHtml::encode($data->goalId); ?>
-	<br />
+// start article
+echo PHtml::openTag('article', array(
+	'class' => $articleClass,		
+));
 
-	<b><?php echo CHtml::encode($data->getAttributeLabel('name')); ?>:</b>
-	<?php echo CHtml::encode($data->name); ?>
-	<br />
+// start headers
+echo PHtml::openTag('header');
+echo PHtml::openTag('hgroup');
 
-	<b><?php echo CHtml::encode($data->getAttributeLabel('ownerId')); ?>:</b>
-	<?php echo CHtml::encode($data->ownerId); ?>
-	<br />
+// event name
+echo PHtml::openTag('h1');
+echo PHtml::encode($data->name);
+echo PHtml::closeTag('h1');
 
-	<b><?php echo CHtml::encode($data->getAttributeLabel('priority')); ?>:</b>
-	<?php echo CHtml::encode($data->priority); ?>
-	<br />
+// event date
+if(isset($data->starts) 
+|| $data->ends) {
+	echo PHtml::openTag('h2');
+	echo PHtml::openTag('time');
+	echo PHtml::encode($data->starts . "-" . $data->ends);
+	echo PHtml::closeTag('time');
+	echo PHtml::closeTag('h2');
+}
 
-	<b><?php echo CHtml::encode($data->getAttributeLabel('isCompleted')); ?>:</b>
-	<?php echo CHtml::encode($data->isCompleted); ?>
-	<br />
+// event attendee count
+echo PHtml::openTag('h2');
+echo PHtml::openTag('span');
+echo $data->userTasksCount;
+echo $data->userTasksCount == 1 
+	? ' person signed up' 
+	: ' people signed up';
+echo PHtml::closeTag('span');
+echo PHtml::closeTag('h2');
 
-	<b><?php echo CHtml::encode($data->getAttributeLabel('isTrash')); ?>:</b>
-	<?php echo CHtml::encode($data->isTrash); ?>
-	<br />
+// close headers
+echo PHtml::closeTag('hgroup');
+echo PHtml::closeTag('header');
 
-	<b><?php echo CHtml::encode($data->getAttributeLabel('starts')); ?>:</b>
-	<?php echo CHtml::encode($data->starts); ?>
-	<br />
+// body
 
-	<b><?php echo CHtml::encode($data->getAttributeLabel('ends')); ?>:</b>
-	<?php echo CHtml::encode($data->ends); ?>
-	<br />
+// end body
 
-	<b><?php echo CHtml::encode($data->getAttributeLabel('created')); ?>:</b>
-	<?php echo CHtml::encode($data->created); ?>
-	<br />
+// start footer
+echo PHtml::openTag('footer');
+// FIXME: needs a menu
+echo PHtml::link('menu to go here');
+echo PHtml::closeTag('footer');
 
-	<b><?php echo CHtml::encode($data->getAttributeLabel('modified')); ?>:</b>
-	<?php echo CHtml::encode($data->modified); ?>
-	<br />	
-	<br />
-
-</div>
+// close article
+echo PHtml::closeTag('article');
