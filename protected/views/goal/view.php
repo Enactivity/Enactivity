bc100b1a572368e00b80e5b003cda7fcd173bc84
ajsharma <ajsharma@f94a5964-8f9d-401d-8ecb-23302dea6c3a>
2011-04-26 05:03:45 +0000

    Removing commented out code

--- protected/views/goal/view.php
+++ protected/views/goal/view.php
@@ -4,23 +4,6 @@
 	
 	$this->pageMenu = MenuDefinitions::goalMenu($model);
 ?>
-<p>
-<?php 
-/**
-	$this->widget('zii.widgets.CDetailView', array(
-	'data'=>$model,
-	'attributes'=>array(
-		'id',
-		'name',
-		'groupId',
-		'ownerId',
-		'isCompleted',
-		'isTrash',
-		'created',
-		'modified',
-	),
-)); **/?>
-</p>
 <?php 
 	foreach ($model->tasks as $task) {
 		echo $this->renderPartial('/task/_view', array('data'=>$task));
