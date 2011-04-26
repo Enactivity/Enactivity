c5e34800fcdc980632fd8e2661031398eaff837b
ajsharma <ajsharma@f94a5964-8f9d-401d-8ecb-23302dea6c3a>
2011-04-26 07:14:55 +0000

    Fixed typo

--- protected/controllers/GoalController.php
+++ protected/controllers/GoalController.php
@@ -55,7 +55,7 @@ class GoalController extends Controller
 	{
 		$goal = $this->loadModel($id);
 		$tasks = new CActiveDataProvider(
-			'EventBanter', 
+			'Task', 
 			array('data' => $goal->tasks)
 		);
 		
