986ec6bb9bbcf508e050629f769a7933117a5817
ajsharma <ajsharma@f94a5964-8f9d-401d-8ecb-23302dea6c3a>
2011-04-27 03:51:26 +0000

    Commented out image from front page

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -2,11 +2,11 @@
 $this->pageTitle = 'Coming soon'; 
 
 
-$imagePath = Yii::app()->getAssetManager()->publish(
-	Yii::getPathOfAlias('application.views.site.assets')
-	. '/poncla-coming-soon.png'
-);
-echo PHtml::image($imagePath,  
-	'Poncla is under construction.'
-	. '  Please use the time to brace yourself for awesomeness'
-);
+//$imagePath = Yii::app()->getAssetManager()->publish(
+//	Yii::getPathOfAlias('application.views.site.assets')
+//	. '/poncla-coming-soon.png'
+//);
+//echo PHtml::image($imagePath,  
+//	'Poncla is under construction.'
+//	. '  Please use the time to brace yourself for awesomeness'
+//);
