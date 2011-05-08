02f6fbd98de737001e4bd0f55a58ce35fbda5327
ajsharma <ajsharma@f94a5964-8f9d-401d-8ecb-23302dea6c3a>
2011-05-08 23:19:28 +0000

    Added handler for incorrectly called task forms Fixed bug where forgot to use $this-> when calling goal

--- protected/extensions/widgets/forms/TaskForm.php
+++ protected/extensions/widgets/forms/TaskForm.php
@@ -14,17 +14,21 @@ class TaskForm extends CWidget {
 	public function init() {
 		parent::init();
 		
+		if($this->goal == null) {
+			throw new Exception("Dev didn't pass in Goal object to TaskForm widget");
+		}
+		
 		if(is_null($this->task)) {
 			$this->task = new Task();
 		}
-		$this->task->goalId = $goal->id;
+		$this->task->goalId = $this->goal->id;
 	}
 	
 	public function run() {
 		Yii::app()->controller->renderPartial('/task/_form', array(
 			'model' => $this->task,
 			'goal' => $this->goal,
-			'action' => array('/goal/createtask', 'id' => $goal->id),
+			'action' => array('/goal/createtask', 'id' => $this->task->goalId),
 		));
 	}
 }
