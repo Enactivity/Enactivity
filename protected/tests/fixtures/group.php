5a59a76625494566a2c1fd77b17d8f78064211ed
ajsharma <ajsharma@f94a5964-8f9d-401d-8ecb-23302dea6c3a>
2011-05-15 21:40:22 +0000

    Refactoring fixture file case

---
+++ protected/tests/fixtures/group.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Fixtures for group
+ */
+return array(
+	'testgroup' => array(
+		'name' => 'This is a Test Group',
+		'slug' => 'test',
+		'created' => date ("Y-m-d H:i:s", strtotime("-1 hours")),
+		'modified' => date ("Y-m-d H:i:s", strtotime("-1 hours")),
+	),
+);
