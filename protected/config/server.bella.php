078f06cd01bcf1ef0e2ab714990e115cb78fd2a5
ajsharma <ajsharma@f94a5964-8f9d-401d-8ecb-23302dea6c3a>
2011-05-27 04:03:13 +0000

    Restored inheritance from production Removed excess db params

--- protected/config/server.bella.php
+++ protected/config/server.bella.php
@@ -1,20 +1,16 @@
 <?php
 // This is the test Web application configuration. Any writable
 // CWebApplication properties can be configured here.
-// Overrides any settings from main.inc.php
 return CMap::mergeArray(
-	require(dirname(__FILE__).'/main.inc.php'),
+	require(dirname(__FILE__).'/server.production.php'),
 	array(
 		'components'=>array(
 			
 			// MySQL database settings for bella
 			'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=poncla_yii',
-			'emulatePrepare' => true,
-			'username' => 'root',
-			'password' => 'justr1d3',
-			'charset' => 'utf8',
-			'enableParamLogging'=>false
+				'connectionString' => 'mysql:host=localhost;dbname=poncla_yii',
+				'username' => 'root',
+				'password' => 'justr1d3',
 			),
 		),
 	)
