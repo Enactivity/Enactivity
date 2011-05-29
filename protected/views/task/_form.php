babf7ddb2c3399c8c6587da33977af1a1443233d
ajsharma <ajsharma@f94a5964-8f9d-401d-8ecb-23302dea6c3a>
2011-05-29 22:06:47 +0000

    Removed strtotime formatting

--- protected/views/task/_form.php
+++ protected/views/task/_form.php
@@ -32,7 +32,6 @@ $form=$this->beginWidget('application.components.widgets.ActiveForm', array(
 		<?php echo $form->labelEx($model,'starts'); ?>
 		<div class="forminput"><?php
 			// preformat date before loading into widget 
-			$model->starts = isset($model->starts) ? date('m/d/Y g:i a', strtotime($model->starts)) : null;
 			$this->widget('application.components.widgets.jui.JuiDateTimePicker', 
 				array(
 					'model'=>$model,
@@ -53,7 +52,6 @@ $form=$this->beginWidget('application.components.widgets.ActiveForm', array(
 		<?php echo $form->labelEx($model,'ends'); ?>
 		<div class="forminput"><?php
 			// preformat date before loading into widget 
-			$model->starts = isset($model->starts) ? date('m/d/Y g:i a', strtotime($model->starts)) : null;
 			$this->widget('application.components.widgets.jui.JuiDateTimePicker', 
 				array(
 					'model'=>$model,
