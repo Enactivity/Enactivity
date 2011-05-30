5e2c9485582dfad2e831aeeed565a85e95aa6a04
ajsharma <ajsharma@f94a5964-8f9d-401d-8ecb-23302dea6c3a>
2011-05-30 01:45:24 +0000

    Adding poncla-splash to site/index

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,12 +1,2 @@
 <?php 
-$this->pageTitle = 'Coming soon'; 
-
-
-//$imagePath = Yii::app()->getAssetManager()->publish(
-//	Yii::getPathOfAlias('application.views.site.assets')
-//	. '/poncla-coming-soon.png'
-//);
-//echo PHtml::image($imagePath,  
-//	'Poncla is under construction.'
-//	. '  Please use the time to brace yourself for awesomeness'
-//);
+$this->pageTitle = 'Discover a better way to interact with your group'; 
