980f1e21797bad3b9cb2e9ba512ebc016f9add54
ajsharma <ajsharma@f94a5964-8f9d-401d-8ecb-23302dea6c3a>
2011-06-15 06:32:33 +0000

    Removed permalink from group view

--- protected/views/group/view.php
+++ protected/views/group/view.php
@@ -4,27 +4,12 @@ $this->pageTitle = $model->name;
 $this->pageMenu = MenuDefinitions::groupMenu($model);
 ?>
 
-<?php 
-
-$this->widget('application.components.widgets.DetailView', array(
-	'data'=>$model,
-	'attributes'=>array(
-		array(
-			'name' => 'About Us',
-			'value' => $model->groupProfile->description,
-			'type' => 'styledtext',
-			'visible' => strlen($model->groupProfile->description) > 0 ? true : false,
-			
-		),
-		'permalink:url',
-	),
-)); 
-?>
-
 <!-- List of users in group -->
 <?php if(!Yii::app()->user->isGuest):?>
 <section id="users">
-	<h1><?php echo $activemembers->getTotalItemCount() . ' Active Members'; ?></h1>
+	<hgroup>
+		<h1><?php echo $activemembers->getTotalItemCount() . ' Active Members'; ?></h1>
+	</hgroup>
 	
 	<?php 
 	$this->widget('zii.widgets.CListView', array(
