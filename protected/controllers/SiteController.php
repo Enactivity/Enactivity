8268c473d7bad995a84e94ea24835520fba12247
ajsharma <ajsharma@f94a5964-8f9d-401d-8ecb-23302dea6c3a>
2011-06-22 05:53:21 +0000

    Adding contact form to index

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -29,13 +29,42 @@ class SiteController extends Controller
 	{
 		// renders the view file 'protected/views/site/index.php' if not logged-in
 		if(Yii::app()->user->isGuest) {
-			$this->render('index');	
+			$model=new ContactForm;
+			if(isset($_POST['ContactForm']))
+			{
+				$model->attributes = $_POST['ContactForm'];
+				if($model->validate())
+				{
+					$model->sendEmail();
+				}
+			}
+			$this->render('index', array('model'=>$model));
 		} 
 		else {
 			$this->redirect(array('task/whatsnext'));
 		}
 	}
 
+	/**
+	 * Displays the contact page
+	 */
+	public function actionContact()
+	{
+		$model=new ContactForm;
+		if(isset($_POST['ContactForm']))
+		{
+			$model->attributes=$_POST['ContactForm'];
+			if($model->validate())
+			{
+				$headers="From: {$model->email}\r\nReply-To: {$model->email}";
+				mail(Yii::app()->params['adminEmail'],$model->subject,$model->body,$headers);
+				Yii::app()->user->setFlash('contact','Thank you for contacting us. We will respond to you as soon as possible.');
+				$this->refresh();
+			}
+		}
+		$this->render('contact',array('model'=>$model));
+	}
+	
 	/**
 	 * This is the action to handle external exceptions.
 	 */
--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,2 +1,29 @@
 <?php 
 $this->pageTitle = 'Discover a better way to interact with your group'; 
+?>
+
+<?php $form=$this->beginWidget('application.components.widgets.ActiveForm', array(
+	'id'=>'contact-form',
+	//'enableClientValidation'=>true,
+	'clientOptions'=>array(
+		'validateOnSubmit'=>true,
+	),
+)); ?>
+
+	<h2>Sign up for news about our launch.</h2>
+
+	<div class="row">
+		<div class="formlabel"><?php echo $form->labelEx($model,'email'); ?></div>
+		<div class="forminput"><?php echo $form->emailField($model,'email', 
+			array(
+				'placeholder'=>'@',
+				'autofocus'=>'autofocus',
+			)); ?></div>
+		<div class="formerrors"><?php echo $form->error($model,'email'); ?></div>
+	</div>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton('Notify me'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
