661742f3f491a9209e2776121b9df25cffc7c071
ajsharma <ajsharma@f94a5964-8f9d-401d-8ecb-23302dea6c3a>
2011-08-28 22:50:07 +0000

    Adding getStartDate tests

---
+++ protected/tests/unit/model/task/GetStartDateTest.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * Tests for Task->getStartDate()
+ */
+
+require_once 'TestConstants.php';
+
+class GetStartDateTest extends DbTestCase
+{
+
+	public $fixtures = array(
+		'groupFixtures'=>':group',
+		'userFixtures'=>':user',
+		'groupUserFixtures'=>':group_user',
+		'task_user'=>':task_user',
+	);
+
+	public $task;
+	
+	public function setUp()
+	{
+		parent::setUp();
+		
+		// login as user
+		$loginForm = new UserLoginForm();
+		$loginForm->email = $this->userFixtures['registered']['email'];
+		$loginForm->password = 'test';
+		$loginForm->login();
+		
+		// create a new task
+		$this->task = new Task();
+		$this->task->name = 'test_' . time();
+		$this->task->saveNode();
+	}
+	
+	/**
+	 * Get start date when starts is null
+	 */
+	public function testGetStartDateWithNoDate() {
+		
+		// try method
+		try {
+			$startDate = $this->task->getStartDate();
+			$this->assertNull($startDate, 'Start date was not null when starts was null');
+		}
+		catch(Exception $e) {
+			$this->fail('getStartDate() with no starts threw exception: ' . $e->getMessage());
+		}
+	}
+	
+	public function testGetStartDateWithNoDateMagic() {
+		// try magic method
+		try {
+			$startDate = $this->task->startDate;
+			$this->assertNull($startDate, 'startDate was not null when starts was null');
+		}
+		catch(Exception $e) {
+			$this->fail('startDate with no starts threw exception: ' . $e->getMessage());
+		}
+	}
+	
+	public function testGetStartDateWithDate() {
+		$now = time();
+		$this->task->starts = date('m/d/Y H:i:00', $now);
+		$this->task->saveNode();
+		
+		// try method
+		try {
+			$startDate = $this->task->getStartDate();
+			$this->assertEquals($startDate, date('Y-m-d', $now), 'Get start date does not match');
+		}
+		catch(Exception $e) {
+			$this->fail('getStartDate() with starts threw exception: ' . $e->getMessage());
+		}
+	}
+	
+	public function testGetStartDateWithDateMagic() {
+		$now = time();
+		$this->task->starts = date('m/d/Y H:i:00', $now);
+		$this->task->saveNode();
+		
+		// try method
+		try {
+			$startDate = $this->task->startDate;
+			$this->assertEquals($startDate, date('Y-m-d', $now), 'Get start date does not match');
+		}
+		catch(Exception $e) {
+			$this->fail('startDate with starts threw exception: ' . $e->getMessage());
+		}
+	}
+	
+}
