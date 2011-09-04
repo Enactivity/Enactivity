ba50ba7c1dd4dd2d5f289f9822c7c670f5c9b5e5
ajsharma <ajsharma@f94a5964-8f9d-401d-8ecb-23302dea6c3a>
2011-09-04 22:56:07 +0000

    Refactoring make and insert users

--- protected/tests/factories/UserFactory.php
+++ protected/tests/factories/UserFactory.php
@@ -6,20 +6,48 @@
 class UserFactory extends AbstractFactory {
 
 	/**
-	 * Returns a new active user in the current group
-	 * @param attributes, overloads default attributes
-	 * @param group to add user to
-	 * @return User unsaved user
+	 * Generate a user who has not yet registered, no group-user made
+	 * @param array $attributes
+	 * @param int $groupId
 	 */
-	private static function make($attributes = array(), $groupId = 1) {
+	static function make($attributes = array()) {
 		// invite
 		$user = new User();
 		$user->scenario = User::SCENARIO_INVITE;
 		$user->attributes = array(
 			'email' => "pemail+" + uniqid() + "@alpha.poncla.com"
 		);
-		$user->save();
-		
+
+		return $user;
+	}
+
+	/**
+	 * Generate a user who has not yet registered
+	 * @param array $attributes
+	 * @param int $groupId
+	 */
+	static function insertInvited($attributes, $groupId = 1) {
+		$user = self::makeInvited($attributes);
+
+		// add user to group
+		$groupUser = new GroupUser();
+		$groupUser->groupId = $groupId;
+		$groupUser->userId = $user->id;
+		$groupUser->status = GroupUser::STATUS_ACTIVE;
+		$groupUser->save();
+
+		return User::model()->findAllByPk($user->id);
+	}
+
+	/**
+	 * Returns a user generated via {@link UserFactory::make()}
+	 * @param attributes, overloads default attributes
+	 * @return User saved user
+	 */
+	static function insert($attributes = array(), $groupId = 1) {
+		// invite
+		$user = self::insertInvited($attributes, $groupId);
+
 		// register
 		$user = User::model()->findById($user->id);
 		$user->scenario = User::SCENARIO_REGISTER;
@@ -28,32 +56,12 @@ class UserFactory extends AbstractFactory {
 			'lastName' => "plast" + uniqid(),
 			'password' => "pw" + uniqid(),
 		);
-		
+
 		// overload attributes
 		$user->attributes = $attributes;
-		
+
 		$user->save();
-		
-		return User::model()->findByPk($user->id);
-	}
-	
-	/**
-	 * Returns a user generated via {@link UserFactory::make()}
-	 * @param attributes, overloads default attributes
-	 * @return User saved user
-	 */
-	static function insert($attributes = array()) {
-		$user = self::make($attributes);
-		
-		$user->saveNode();
-		
-		// add user to group
-		$groupUser = new GroupUser();
-		$groupUser->groupId = $groupId;
-		$groupUser->userId = $user->id;
-		$groupUser->status = GroupUser::STATUS_ACTIVE;
-		$groupUser->save();
-		
+
 		return User::model()->findByPk($user->id);
 	}
 }
