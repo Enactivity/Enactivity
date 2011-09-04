2bdf77c523bd7d6f8bd23ffde8c2a739a7438e37
ajsharma <ajsharma@f94a5964-8f9d-401d-8ecb-23302dea6c3a>
2011-09-04 20:15:36 +0000

    Adding tests for Task->CTimestampBehavior

---
+++ protected/tests/unit/model/task/behaviors/CTimestampBehaviorTest.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * Tests related to the initial insertion of a Task model
+ * @author ajsharma
+ */
+class CTimestampBehaviorTest extends DbTestCase {
+
+	var $task;
+
+	public function setUp() {
+		parent::setUp();
+
+		// test parameters
+		$name = "test task";
+		$isTrash = 0;
+		$created = date ("Y-m-d H:i:s", strtotime("-1 hours"));
+		$modified = date ("Y-m-d H:i:s", strtotime("-1 hours"));
+
+		// run test
+		$this->task = new Task();
+		$this->task->attributes = array(
+					'id' => $id,
+					'name' => $name,
+					'isTrash' => $isTrash,
+					'created' => $created,
+					'modified' => $modified,
+		);
+	}
+
+	/**
+	 * Test that created timestamp is set
+	 */
+	public function testCreatedSetOnInsert() {
+
+		// confirm datetimes are not set
+		$this->assertNull($this->task->created, 'task created attribute was assigned in [' . $this->task->getScenario() . ']');
+
+		$this->task->saveNode();
+
+		// confirm datetime set
+		$this->assertNotNull($this->task->created, 'task created attribute was not assigned in [' . $this->task->getScenario() . ']');
+	}
+	
+	/**
+	 * Test that created is set to current time
+	 */
+	public function testCreatedSetCorrectly() {
+		$now = time();
+		
+		$this->task->saveNode();
+		
+		$later = time();
+		
+		$task = Task::model()->findByPk($this->task->id);
+		$created = strtotime($task->created);
+		
+		$this->assertGreaterThanOrEqual($now, $created, "Task created is earlier than save time");
+		$this->assertLessThanOrEqual($later, $created, "Task created is later than save time");
+	}
+
+	/**
+	 * Test that modified timestamp is set
+	 */
+	public function testModifiedSetOnInsert() {
+
+		// confirm datetimes are not set
+		$this->assertNull($this->task->modified, 'task modified attribute was assigned in [' . $this->task->getScenario() . ']');
+
+		$this->task->saveNode();
+
+		// confirm datetime set
+		$this->assertNotNull($this->task->modified, 'task modified attribute was not assigned in [' . $this->task->getScenario() . ']');
+	}
+
+	/**
+	 * Test that modified timestamp is set on update
+	 */
+	public function testModifiedSetOnUpdate() {
+
+		// insert task
+		$this->task->saveNode();
+		
+		// find task
+		$oldTask = Task::model()->findByPk($this->task->id);
+
+		// update task
+		$oldTask->name = "test task updated";
+		$oldTask->saveNode();
+
+		// find task again
+		$newTask = Task::model()->findByPk($this->task->id);
+		
+		// confirm datetime set
+		$this->assertNotEquals($oldTask->modified, $newTask->modified, "task modified attribute was not updated on update");
+	}
+
+}
