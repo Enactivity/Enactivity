598679c5d8a9cf03f082754bc1c2d57241dec318
ajsharma <ajsharma@f94a5964-8f9d-401d-8ecb-23302dea6c3a>
2011-09-05 18:40:45 +0000

    Fixed broken default

--- protected/views/task/_agenda.php
+++ protected/views/task/_agenda.php
@@ -8,7 +8,7 @@
 // instantiate the arrays if needed
 $datedTasks = empty($datedTasks) ? array() : $datedTasks;
 $datelessTasks = empty($datelessTasks) ? array() : $datelessTasks;
-$showParent = empty($showParent) ? true : $showParent;
+$showParent = empty($showParent) ? $showParent : true;
 
 ?>
 <section class='agenda'>
