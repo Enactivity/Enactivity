c28b35c66cda4a5a43806441c78168157fb639de
ajsharma <ajsharma@f94a5964-8f9d-401d-8ecb-23302dea6c3a>
2011-09-18 18:40:47 +0000

    Removing unused widgets

--- protected/views/groupbanter/create.php
+++
@@ -1,6 +0,0 @@
-<?php
-$this->pageTitle = "Banter";
-
-?>
-
-<?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
--- protected/views/groupbanter/update.php
+++
@@ -1,7 +0,0 @@
-<?php
-$this->pageTitle = StringUtils::truncate($model->content, 60);
-
-$this->pageMenu = MenuDefinitions::groupBanterMenu($model);
-?>
-
-<?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
