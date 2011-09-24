af1dc03c8945e1007ea6d1f4a7d8a246caa1571d
ajsharma <ajsharma@f94a5964-8f9d-401d-8ecb-23302dea6c3a>
2011-09-24 20:13:55 +0000

    Removing remember me and defaulting to remembering

--- protected/models/UserLoginForm.php
+++ protected/models/UserLoginForm.php
@@ -101,7 +101,8 @@ class UserLoginForm extends CFormModel
 		}
 		if($this->_identity->errorCode === UserIdentity::ERROR_NONE)
 		{
-			$duration = $this->rememberMe ? 3600*24*30 : 0; // 30 days
+			//$duration = $this->rememberMe ? 3600*24*30 : 0; // 30 days
+			$duration = 3600*24*30; // 30 days
 			Yii::app()->user->login($this->_identity, $duration);
 			//TODO: update last login of user
 			return true;
--- protected/views/site/login.php
+++ protected/views/site/login.php
@@ -28,12 +28,6 @@ $this->pageTitle = 'Login';
 		<div class="formerrors"><?php echo $form->error($model,'password'); ?></div>
 	</div>
 
-	<div class="field rememberMe">
-		<div class="formlabel"><?php echo $form->labelEx($model,'rememberMe'); ?></div>
-		<div class="forminput"><?php echo $form->checkBox($model,'rememberMe'); ?></div>
-		<div class="formerrors"><?php echo $form->error($model,'rememberMe'); ?></div>
-	</div>
-
 	<div class="field buttons">
 		<?php echo PHtml::submitButton('Login'); ?>
 	</div>
