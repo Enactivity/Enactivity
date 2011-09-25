e2be0f6d41b1d275b5f9f7f8ce5c1d0108cac6ac
ajsharma <ajsharma@f94a5964-8f9d-401d-8ecb-23302dea6c3a>
2011-09-25 21:18:18 +0000

    Added preventDefault to clearDateTime script

--- protected/components/widgets/jui/assets/clearDateTime.js
+++ protected/components/widgets/jui/assets/clearDateTime.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 	$('.clear-date-time').live('click',function(){
+		event.preventDefault();
 		$(this).siblings(':input').val("");
 		$(this).siblings(':select').val("");
 	});
