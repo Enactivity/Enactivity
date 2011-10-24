32bfa810743a71e7acb902cc55a9019097fb8a27
ajsharma <ajsharma@f94a5964-8f9d-401d-8ecb-23302dea6c3a>
2011-10-24 04:25:40 +0000

    Tests for getstatuses

--- protected/tests/unit/model/user/UserGetStatusesTest.php
+++ protected/tests/unit/model/user/UserGetStatusesTest.php
@@ -9,10 +9,10 @@ class UserGetStatusesTest extends DbTestCase
 	 * Test get statuses returns all User statuses 
 	 */
 	public function testGetStatuses() {
-		$statuses = array(self::STATUS_ACTIVE,
-		self::STATUS_INACTIVE,
-		self::STATUS_PENDING,
-		self::STATUS_BANNED);
+		$statuses = array(User::STATUS_ACTIVE,
+		User::STATUS_INACTIVE,
+		User::STATUS_PENDING,
+		User::STATUS_BANNED);
 		
 		$this->assertEquals($statuses, User::getStatuses());
 	}
