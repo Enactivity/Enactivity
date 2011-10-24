ca36115f4a77023be55150adf7e9b38a2ba37db9
ajsharma <ajsharma@f94a5964-8f9d-401d-8ecb-23302dea6c3a>
2011-10-24 04:15:23 +0000

    Adding tests for isActive and isBanned

--- protected/tests/unit/model/user/UserGetIsActiveTest.php
+++ protected/tests/unit/model/user/UserGetIsActiveTest.php
@@ -6,10 +6,39 @@
 class UserGetIsActiveTest extends DbTestCase
 {
 	/**
-	 * Test  
+	 * Test if active user returns isActive
 	 */
-	public function test() {
-		$this->markTestIncomplete();
+	public function testActiveIsActive() {
+		$user = UserFactory::insert();
+		$user->status = User::STATUS_ACTIVE;
+		$this->assertTrue($user->isActive, "Active status user doesn't return isActive");
 	}
-	
+
+	/**
+	 * Test if banned user returns isActive
+	 */
+	public function testBannedIsActive() {
+		$user = UserFactory::insert();
+		$user->status = User::STATUS_BANNED;
+		$this->assertFalse($user->isActive, "Banned status user returns isActive");
+	}
+
+	/**
+	 * Test if inactive user returns isActive
+	 */
+	public function testInactiveIsActive() {
+		$user = UserFactory::insert();
+		$user->status = User::STATUS_INACTIVE;
+		$this->assertFalse($user->isActive, "Inactive status user returns isActive");
+	}
+
+	/**
+	 * Test if pending user returns isActive
+	 */
+	public function testPendingIsActive() {
+		$user = UserFactory::insert();
+		$user->status = User::STATUS_PENDING;
+		$this->assertFalse($user->isActive, "Pending status user returns isActive");
+	}
+
 }
