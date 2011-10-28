76ac3a529b47e3b5d432e4958c831ac8523eb996
ajsharma <ajsharma@f94a5964-8f9d-401d-8ecb-23302dea6c3a>
2011-10-28 04:04:01 +0000

    Adding isStatus tests

--- protected/tests/unit/model/user/UserIsStatusTest.php
+++ protected/tests/unit/model/user/UserIsStatusTest.php
@@ -5,11 +5,52 @@
  */
 class UserIsStatusTest extends DbTestCase
 {
-	/**
-	 * Test  
+		/**
+	 * Test if active user returns isBanned
 	 */
-	public function test() {
-		$this->markTestIncomplete();
+	public function testActiveisStatus() {
+		$user = UserFactory::insert();
+		$user->status = User::STATUS_ACTIVE;
+		$this->assertTrue($user->isStatus(User::STATUS_ACTIVE), "isStatus did not match status of user");
+		$this->assertFalse($user->isStatus(User::STATUS_BANNED), "isStatus did not match status of user");
+		$this->assertFalse($user->isStatus(User::STATUS_INACTIVE), "isStatus did not match status of user");
+		$this->assertFalse($user->isStatus(User::STATUS_PENDING), "isStatus did not match status of user");
 	}
-	
+
+	/**
+	 * Test if banned user returns isBanned
+	 */
+	public function testBannedisStatus() {
+		$user = UserFactory::insert();
+		$user->status = User::STATUS_BANNED;
+		$this->assertFalse($user->isStatus(User::STATUS_ACTIVE), "isStatus did not match status of user");
+		$this->assertTrue($user->isStatus(User::STATUS_BANNED), "isStatus did not match status of user");
+		$this->assertFalse($user->isStatus(User::STATUS_INACTIVE), "isStatus did not match status of user");
+		$this->assertFalse($user->isStatus(User::STATUS_PENDING), "isStatus did not match status of user");
+	}
+
+	/**
+	 * Test if inactive user returns isBanned
+	 */
+	public function testInactiveisStatus() {
+		$user = UserFactory::insert();
+		$user->status = User::STATUS_INACTIVE;
+		$this->assertFalse($user->isStatus(User::STATUS_ACTIVE), "isStatus did not match status of user");
+		$this->assertFalse($user->isStatus(User::STATUS_BANNED), "isStatus did not match status of user");
+		$this->assertTrue($user->isStatus(User::STATUS_INACTIVE), "isStatus did not match status of user");
+		$this->assertFalse($user->isStatus(User::STATUS_PENDING), "isStatus did not match status of user");
+	}
+
+	/**
+	 * Test if pending user returns isBanned
+	 */
+	public function testPendingisStatus() {
+		$user = UserFactory::insert();
+		$user->status = User::STATUS_PENDING;
+		$this->assertFalse($user->isStatus(User::STATUS_ACTIVE), "isStatus did not match status of user");
+		$this->assertFalse($user->isStatus(User::STATUS_BANNED), "isStatus did not match status of user");
+		$this->assertFalse($user->isStatus(User::STATUS_INACTIVE), "isStatus did not match status of user");
+		$this->assertTrue($user->isStatus(User::STATUS_PENDING), "isStatus did not match status of user");
+	}
+
 }
