09c8f0b4e04158f70dcc94a15c3e3bae4384dba6
ajsharma <ajsharma@f94a5964-8f9d-401d-8ecb-23302dea6c3a>
2011-10-30 21:12:19 +0000

    Adding createRandomNumericString Added default value of 10 for createRandomString

--- protected/helpers/StringUtils.php
+++ protected/helpers/StringUtils.php
@@ -1,48 +1,66 @@
 <?php
 /**
- * Utilities functions for strings 
+ * Utilities functions for strings
  */
 class StringUtils {
-	
+
 	/**
 	 * Create a random string of letters and numbers
 	 * @param integer $length number of characters in string
 	 */
-	public static function createRandomString($length) { 
+	public static function createRandomString($length = 10) {
 		$chars = "abcdefghijkmnopqrstuvwxyz";
 		$chars .= "ABCDEFGHIJKMNOPQRSTUVWXYZ";
-		$chars .= "023456789";
+		$chars .= "0123456789";
 		srand((double) microtime() * 1000000);
 		$randomString = '';
-		
+
 		for($i = 0; $i < $length; $i++) {
 			$index = rand() % 33;
 			$tmp = substr($chars, $index, 1);
 			$randomString = $randomString . $tmp;
 		}
-		
+
 		return $randomString;
-	} 
-	
+	}
+
 	/**
-	* Create a random string of letters and numbers
-	* @param integer $length number of characters in string
-	*/
+	 * Create a random string of letters and numbers
+	 * @param integer $length number of characters in string
+	 */
 	public static function createRandomAlphaString($length = 10) {
 		$chars = "abcdefghijkmnopqrstuvwxyz";
 		$chars .= "ABCDEFGHIJKMNOPQRSTUVWXYZ";
 		srand((double) microtime() * 1000000);
 		$randomString = '';
-	
+
 		for($i = 0; $i < $length; $i++) {
 			$index = rand() % 33;
 			$tmp = substr($chars, $index, 1);
 			$randomString = $randomString . $tmp;
 		}
-	
+
 		return $randomString;
 	}
-	
+
+	/**
+	 * Create a random string of numbers
+	 * @param integer $length number of characters in string
+	 */
+	public static function createRandomNumericString($length = 10) {
+		$chars = "0123456789";
+		srand((double) microtime() * 1000000);
+		$randomString = '';
+
+		for($i = 0; $i < $length; $i++) {
+			$index = rand() % 33;
+			$tmp = substr($chars, $index, 1);
+			$randomString = $randomString . $tmp;
+		}
+
+		return $randomString;
+	}
+
 	/**
 	 * Returns a 23 character unique string
 	 * @return String
@@ -52,20 +70,20 @@ class StringUtils {
 		$string = str_replace('.', '', $string);
 		return $string;
 	}
-	
+
 	/**
-	 * Truncates the given string to a set length, with the suffix if 
+	 * Truncates the given string to a set length, with the suffix if
 	 * the initial string was longer than the length.
 	 * @param string $string
 	 * @param int $length defaults to 64
-	 * @param string $suffix defaults to '...' 
+	 * @param string $suffix defaults to '...'
 	 * @return string of provided length
 	 */
 	public static function truncate($string, $length = 64, $suffix = '...') {
 		if(strlen($suffix) > $length) {
 			throw new CHttpException("Truncation suffix is longer than length");
 		}
-		
+
 		if(strlen($string) > $length) {
 			$string = substr($string, 0, $length - strlen($suffix));
 			$string = trim($string);
@@ -73,7 +91,7 @@ class StringUtils {
 		}
 		return $string;
 	}
-	
+
 	/**
 	 * Create a random email address
 	 * @return String email address
@@ -81,6 +99,6 @@ class StringUtils {
 	public static function createRandomEmail() {
 		return "pemail+" . StringUtils::uniqueString() . "@alpha.poncla.com";
 	}
-	
+
 }
 ?>
