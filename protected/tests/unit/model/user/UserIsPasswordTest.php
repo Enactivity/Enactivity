440493a15ce367667e4a6182602c63860747ade0
ajsharma <ajsharma@f94a5964-8f9d-401d-8ecb-23302dea6c3a>
2011-10-31 03:14:01 +0000

    tests for IsPassword

--- protected/tests/unit/model/user/UserIsPasswordTest.php
+++ protected/tests/unit/model/user/UserIsPasswordTest.php
@@ -6,10 +6,29 @@
 class UserIsPasswordTest extends DbTestCase
 {
 	/**
-	 * Test  
+	 * Test isPassword works when password match
 	 */
-	public function test() {
-		$this->markTestIncomplete();
+	public function testIsPasswordValid() {
+		$password = StringUtils::createRandomAlphaString();
+		$user = UserFactory::insert(array(
+			'password' => $password,
+		));
+		
+		$this->assertTrue($user->isPassword($password), "Password does not match saved password");
+		$this->assertFalse($user->isPassword(StringUtils::createRandomAlphaString()), "Password matches random string");
+	}
+	
+	/**
+	 * Verify that isPassword breaks if the user's token changes.
+	 */
+	public function testIsPasswordBreaksOnTokenChange() {
+		$password = StringUtils::createRandomAlphaString();
+		$user = UserFactory::insert(array(
+				'password' => $password,
+		));
+		
+		$user->token = StringUtils::createRandomString();
+		$this->assertFalse($user->isPassword($password), "Password matches password after token changes");
 	}
 	
 }
