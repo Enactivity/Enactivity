566089b819d2a0f8c39a0660f414cc31a1d9da85
ajsharma <ajsharma@f94a5964-8f9d-401d-8ecb-23302dea6c3a>
2011-11-13 23:13:37 +0000

    Adding section wrapper

--- protected/views/group/index.php
+++ protected/views/group/index.php
@@ -7,8 +7,10 @@ $this->pageTitle = 'Groups';
 	<h1><?php echo PHtml::encode($this->pageTitle);?></h1>
 <?php echo PHtml::endContentHeader(); ?>
 
+<section class="novel">
 <?php $this->widget('zii.widgets.CListView', array(
 	'dataProvider'=>$dataProvider,
 	'itemView'=>'_view',
 	'cssFile'=>false,
 )); ?>
+<section>
