6afb9908528ffea003de217724c00eb86f4f5e7a
hvuong <hvuong@f94a5964-8f9d-401d-8ecb-23302dea6c3a>
2011-12-04 22:51:37 +0000

    fix for not being able to clear datetime input in firefox

--- protected/components/widgets/jui/assets/clearDateTime.js
+++ protected/components/widgets/jui/assets/clearDateTime.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
-	$('.clear-date-time').live('click',function(){
-		event.preventDefault();
-		$(this).siblings(':input').val("");
-		$(this).siblings(':select').val("");
+	$('.clear-date-time').click(function(event){
+		$(this).siblings('input').val("");
+		$(this).siblings('select').val("");
+		return false;
 	});
 });
