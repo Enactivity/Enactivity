7dc5ca9cd7de29f9a4db0cd920bd91845c3ea575
ajsharma <ajsharma@f94a5964-8f9d-401d-8ecb-23302dea6c3a>
2012-01-20 06:02:46 +0000

    Adding styling and block format to greeting

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -3,7 +3,7 @@ $this->pageTitle = 'Discover a better way to collaborate with your group';
 ?>
 
 <header class="greetings">
-	<h1>If you have ever doubted that your small group can change the world. We want to help.</h1>
+	<h1>If you have ever doubted that your small group can change the world.<span>We want to help.</span></h1>
 </header>
 <div class="content">
 </div>
