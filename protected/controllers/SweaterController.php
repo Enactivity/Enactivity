6a29a56832b8e6cc17a28129d82375c7b1ac777a
ajsharma <ajsharma@f94a5964-8f9d-401d-8ecb-23302dea6c3a>
2012-05-06 19:53:29 +0000

    Update sweater availability form

--- protected/controllers/SweaterController.php
+++ protected/controllers/SweaterController.php
@@ -26,7 +26,7 @@ class SweaterController extends Controller
 			),
 			array('allow', // allow admin user to perform 'admin' and 'delete' actions
 				'actions'=>array('admin','create','delete','view','update'),
-				'users'=>array('admin'),
+				'expression'=>'$user->isAdmin',
 			),
 			array('deny',  // deny all users
 				'users'=>array('*'),
--- protected/views/sweater/_form.php
+++ protected/views/sweater/_form.php
@@ -1,72 +1,49 @@
-<div class="form">
-
-<?php $form=$this->beginWidget('CActiveForm', array(
+<?php $form=$this->beginWidget('application.components.widgets.ActiveForm', array(
 	'id'=>'sweater-form',
 	'enableAjaxValidation'=>false,
 )); ?>
 
-	<p class="note">Fields with <span class="required">*</span> are required.</p>
-
 	<?php echo $form->errorSummary($model); ?>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'id'); ?>
-		<?php echo $form->textField($model,'id',array('size'=>10,'maxlength'=>10)); ?>
-		<?php echo $form->error($model,'id'); ?>
+	<div class="field">
+		<b><?php echo PHtml::encode($model->getAttributeLabel('id')); ?>:</b>
+		<?php echo PHtml::encode($model->id); ?>
 	</div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'style'); ?>
-		<?php echo $form->textField($model,'style',array('size'=>20,'maxlength'=>20)); ?>
-		<?php echo $form->error($model,'style'); ?>
+	<div class="field">
+		<b><?php echo PHtml::encode($model->getAttributeLabel('style')); ?>:</b>
+		<?php echo PHtml::encode($model->style); ?>
 	</div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'clothColor'); ?>
-		<?php echo $form->textField($model,'clothColor',array('size'=>50,'maxlength'=>50)); ?>
-		<?php echo $form->error($model,'clothColor'); ?>
+	<div class="field">
+		<b><?php echo PHtml::encode($model->getAttributeLabel('clothColor')); ?>:</b>
+		<?php echo PHtml::encode($model->clothColor); ?>
 	</div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'letterColor'); ?>
-		<?php echo $form->textField($model,'letterColor',array('size'=>50,'maxlength'=>50)); ?>
-		<?php echo $form->error($model,'letterColor'); ?>
+	<div class="field">
+		<b><?php echo PHtml::encode($model->getAttributeLabel('letterColor')); ?>:</b>
+		<?php echo PHtml::encode($model->letterColor); ?>
+
 	</div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'stitchingColor'); ?>
-		<?php echo $form->textField($model,'stitchingColor',array('size'=>50,'maxlength'=>50)); ?>
-		<?php echo $form->error($model,'stitchingColor'); ?>
+	<div class="field">
+		<b><?php echo PHtml::encode($model->getAttributeLabel('stitchingColor')); ?>:</b>
+		<?php echo PHtml::encode($model->stitchingColor); ?>
 	</div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'size'); ?>
-		<?php echo $form->textField($model,'size',array('size'=>5,'maxlength'=>5)); ?>
-		<?php echo $form->error($model,'size'); ?>
+	<div class="field">
+		<b><?php echo PHtml::encode($model->getAttributeLabel('size')); ?>:</b>
+		<?php echo PHtml::encode($model->size); ?>
 	</div>
 
-	<div class="row">
+	<div class="field">
 		<?php echo $form->labelEx($model,'available'); ?>
-		<?php echo $form->textField($model,'available'); ?>
+		<?php echo $form->checkBox($model,'available'); ?>
 		<?php echo $form->error($model,'available'); ?>
 	</div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'created'); ?>
-		<?php echo $form->textField($model,'created'); ?>
-		<?php echo $form->error($model,'created'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'modified'); ?>
-		<?php echo $form->textField($model,'modified'); ?>
-		<?php echo $form->error($model,'modified'); ?>
-	</div>
-
-	<div class="row buttons">
-		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
+	<div class="field buttons">
+		<?php echo PHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
 	</div>
 
 <?php $this->endWidget(); ?>
-
-</div><!-- form -->
