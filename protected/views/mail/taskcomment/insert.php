1f3a0f5d34184a25c6c0170e947fea6c8fd6106f
hvuong <hvuong@f94a5964-8f9d-401d-8ecb-23302dea6c3a>
2012-05-07 00:47:18 +0000

    -added a fix for inserting a new comment

--- protected/views/mail/taskcomment/insert.php
+++ protected/views/mail/taskcomment/insert.php
@@ -29,11 +29,7 @@ echo PHtml::openTag('p');
 //Yo! [user] just left a comment for [taskName]
 //echo "Yo! " . PHtml::encode($user->fullName) . " just left a comment for " . PHtml::encode($data->task->name) . "." ;
 
-foreach($changedAttributes as $header)
-{
-	//todo: taskcomment getTask
-	echo "Yo! " . PHtml::encode($user->fullName) . " just left a comment for " . PHtml::encode($data->getModelObject()->name) . "." ;
-}
+echo "Yo! " . PHtml::encode($user->fullName) . " just left a comment for " . PHtml::encode($data->getModelObject()->name) . "." ;
 
 echo PHtml::closeTag('article');
 ?>
