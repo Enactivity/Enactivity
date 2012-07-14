b0085262dbfb8b7910ddb3a34ebfb5d6eff1cc40
Ajay Sharma <dudleydragon@hotmail.com>
2012-07-13 21:29:22 -0700

    Adding comment body to email for insert

--- protected/views/mail/taskcomment/insert.php
+++ protected/views/mail/taskcomment/insert.php
@@ -3,31 +3,14 @@
  * View for taskcomment model insert scenario
  * 
  */
-
-// calculate article class
-$articleClass = "view";
-$articleClass .= " email";
-
-// start article
-echo PHtml::openTag('article', array(
-	'class' => 'email'
-));
-
-// created date
-echo PHtml::openTag('p');
-echo PHtml::openTag('strong');
-echo PHtml::openTag('time');
-echo PHtml::encode(
-	//FIXME: use actual event time
-	Yii::app()->format->formatDateTime(time())
-);
-echo PHtml::closeTag('time');
-echo PHtml::closeTag('strong');
-echo PHtml::closeTag('p');
-echo PHtml::openTag('p');
-
-//Yo! [user] just left a comment for [taskName]
-echo "Yo! " . PHtml::encode($user->fullName) . " just left a comment for " . PHtml::link(PHtml::encode($data->getModelObject()->name), PHtml::taskURL($data->getModelObject())) . "." ;
-
-echo PHtml::closeTag('article');
 ?>
+<article class="view email">
+	<p>
+		<strong><time><?php echo PHtml::encode(
+			//FIXME: use actual event time
+			Yii::app()->format->formatDateTime(time())
+		); ?></time></strong>
+	</p>
+	<p>Yo! <?php PHtml::e($user->fullName); ?> just left a comment for <?php PHtml::link(PHtml::encode($data->getModelObject()->name), PHtml::taskURL($data->getModelObject())); ?>.</p>
+	<blockquote><p><?php PHtml::e($data->content); ?></p></blockquote>
+</article>
