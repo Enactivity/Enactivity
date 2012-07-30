a8ff77c04dca7d12ac04af87554b625d5d15def8
Ajay Sharma <dudleydragon@hotmail.com>
2012-07-29 19:39:00 -0700

    Making AR abstract class

--- protected/components/ActiveRecord.php
+++ protected/components/ActiveRecord.php
@@ -3,6 +3,7 @@
  * ActiveRecord is the customized base active record class.
  * All model classes for this application should extend from this base class.
  **/
-class ActiveRecord extends CActiveRecord {
+abstract class ActiveRecord extends CActiveRecord {
+
 	
 }
