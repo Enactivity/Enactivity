7910f3e32e32c0bd6c1adf3e538b5e67cd8939ae
Ajay Sharma <dudleydragon@hotmail.com>
2012-07-29 17:06:33 -0700

    Adding inital Facebook libraries and wrapper

---
+++ protected/extensions/PFacebook.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * Facebook connection application component.
+ */
+Yii::import('ext.vendors.facebook.*');
+
+class PFacebook extends CApplicationComponent {
+{
+
+	/**
+	 * @var string Facebook application id.
+	 */
+	public $appID;
+	/**
+	 * @var string Facebook application secret.
+	 */
+	public $appSecret;
+	/**
+	 * @var string the application namespace.
+	 */
+	public $appNamespace;
+	/**
+	 * @var boolean whether file uploads are enabled.
+	 */
+	public $fileUpload;
+
+	protected $_facebook;
+
+	/**
+	 * Initializes this component.
+	 */
+	public function init()
+	{
+		$config = array(
+			'appId' => $this->appID,
+			'secret' => $this->appSecret
+		);
+
+		if ($this->fileUpload !== null)
+			$config['fileUpload'] = $this->fileUpload;
+
+		$this->_facebook = new Facebook($config);
+	}
+
+	/**
+	 * Registers an Open Graph action with Facebook.
+	 * @param string $action the action to register.
+	 * @param array $params the query parameters.
+	 */
+	public function registerAction($action, $params=array())
+	{
+		$this->api('me/'.$this->appNamespace.':'.$action, $params);
+	}
+
+	/**
+	 * Calls the Facebook API.
+	 * @param string $query the query to send.
+	 * @param array $params the query parameters.
+	 * @return array the response.
+	 */
+	public function api($query, $params=array())
+	{
+		$data = array();
+
+		if ($params !== array()) {
+			$query .= '?'.http_build_query($params);
+		}
+
+		try {
+			$data = $this->_facebook->api('/'.$query);
+		}
+		catch (FacebookApiException $e)
+		{
+			//TODO: Throw casted exception
+		}
+
+		return $data;
+	}
+
+	/**
+	 * Returns the locale based on the application language.
+	 * @return string the locale.
+	 */
+	public function getLocale()
+	{
+		$language = Yii::app()->language;
+
+		if ($language !== null)
+		{
+			$pieces = explode('_', $language);
+			if (count($pieces) === 2)
+				return $pieces[0].'_'.strtoupper($pieces[1]);
+		}
+
+		return 'en_US';
+	}
+
+	/**
+	 * Returns the Facebook application instance.
+	 * @return Facebook the instance.
+	 */
+	public function getFacebook()
+	{
+		return $this->_facebook;
+	}
+}
