b2403addc44b85c111d4fd9be23fb724859d8fdf
Ajay Sharma <dudleydragon@hotmail.com>
2012-08-05 13:39:22 -0700

    Adding focalModelName column to ActiveRecordLog

---
+++ protected/migrations/m120805_192844_activerecordlog_name.php
@@ -0,0 +1,42 @@
+<?php
+
+class m120805_192844_activerecordlog_name extends CDbMigration
+{
+	// public function up()
+	// {
+	// }
+
+	// public function down()
+	// {
+	// 	echo "m120805_192844_activerecordlog_name does not support migration down.\n";
+	// 	return false;
+	// }
+
+	
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+		$this->addColumn('activerecordlog', 'focalModelName', 'TEXT NOT NULL AFTER  `focalModelId`');
+
+		$log = new ActiveRecordLog();
+		$logs = $log->findAll();
+
+		foreach ($logs as $log) {
+			echo "\n Updating log: " . $log->id;
+
+			$model = $log->focalModelObject;
+			
+			$log->focalModelName = $model->name;
+			$log->save();
+			
+			unset($model);
+		}
+		echo "\n done";
+	}
+
+	public function safeDown()
+	{
+		return false;
+	}
+	
+}
--- protected/views/feed/_view.php
+++ protected/views/feed/_view.php
@@ -22,7 +22,7 @@ $story = $this->beginWidget('application.components.widgets.Story', array(
 				echo PHtml::encode(strtolower($data->modelObject->getScenarioLabel($data->action)));
 				echo ' ';
 				echo PHtml::link(
-					StringUtils::truncate(PHtml::encode($data->modelObject->feedAttribute), 80),
+					StringUtils::truncate(PHtml::encode($data->focalModelName), 80),
 					array(strtolower($data->focalModel) . '/view', 'id'=>$data->focalModelId)
 				);
 			}
