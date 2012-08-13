5541bf977bfe79906f80217c8b6957c814fb910c
Ajay Sharma <dudleydragon@hotmail.com>
2012-08-12 18:34:44 -0700

    Moving safeUp to up

--- protected/migrations/m120805_192844_activerecordlog_name.php
+++ protected/migrations/m120805_192844_activerecordlog_name.php
@@ -2,9 +2,43 @@
 
 class m120805_192844_activerecordlog_name extends CDbMigration
 {
-	// public function up()
-	// {
-	// }
+	public function up()
+	{
+		$this->addColumn('activerecordlog', 'focalModelName', 'TEXT NOT NULL AFTER  `focalModelId`');
+
+		try {
+			$log = new ActiveRecordLog();
+			$logs = $log->findAll();
+
+			foreach ($logs as $log) {
+				echo "\n Updating log: " . $log->id;
+				
+				$model = $log->focalModelObject;
+				$log->focalModelName = $model ? $model->name : "Something";
+				echo "\n name: " . $log->focalModelName;
+				$log->save();
+
+				echo "\n before: " . memory_get_usage();
+				
+				if($model) {
+					$model->detachBehaviors();
+					unset($model);
+				}
+
+				$log->detachBehaviors();
+				$log->unsetModels();
+				unset($log);
+
+				echo "\n  after: " . memory_get_usage();
+			}
+		}
+		catch(Exception $e) {
+			echo $e;
+			return false;
+		}
+
+		echo "\n done";
+	}
 
 	// public function down()
 	// {
@@ -13,33 +47,8 @@ class m120805_192844_activerecordlog_name extends CDbMigration
 	// }
 
 	
-	// Use safeUp/safeDown to do migration with transaction
-	public function safeUp()
+	public function up()
 	{
-		$this->addColumn('activerecordlog', 'focalModelName', 'TEXT NOT NULL AFTER  `focalModelId`');
 
-		$log = new ActiveRecordLog();
-		$logs = $log->findAll();
-
-		foreach ($logs as $log) {
-			echo "\n Updating log: " . $log->id;
-			
-			$log->focalModelName = $log->focalModelObject->name;
-			$log->save();
-
-			echo "\n before: " . memory_get_usage();
-			
-			$log->unsetModels();
-			unset($log);
-
-			echo "\n  after: " . memory_get_usage();
-		}
-		echo "\n done";
-	}
-
-	public function safeDown()
-	{
-		return false;
-	}
-	
+	}	
 }
