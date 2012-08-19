e951dcab3ce472050f804cd1412a1e9eb823c09c
Ajay Sharma <dudleydragon@hotmail.com>
2012-08-19 15:49:03 -0700

    Adding tests for task logging

--- protected/tests/unit/model/task/behaviors/TaskActiveRecordLogBehaviorTest.php
+++ protected/tests/unit/model/task/behaviors/TaskActiveRecordLogBehaviorTest.php
@@ -9,52 +9,42 @@ class TaskActiveRecordLogBehavior extends DbTestCase {
 		parent::setUp();
 	}
 
-	const SCENARIO_DELETE = 'delete';
-	const SCENARIO_INSERT = 'insert'; // default set by Yii
-	const SCENARIO_TRASH = 'trash';
-	const SCENARIO_UNTRASH = 'untrash';
-	const SCENARIO_UPDATE = 'update'; // default set by Yii
-
-	public function testScenarioDelete() {
-		$this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
-	}
-
 	/**
 	 * Test the a new task has the inital feed item.
 	 **/
 	public function testScenarioInsertInitialFeed() {
 		$task = TaskFactory::insert();
-		$task->refresh();
 		$feed = $task->feed;
+		
 		$this->assertEquals(1, count($feed), 'Task has more than one feed item on insert');
 	}
 
 	public function testScenarioInsertInitialFeedNameMatch() {
 		$task = TaskFactory::insert();
-		$task->refresh();
 		$feed = $task->feed;
-		$this->assertEquals(1, count($feed), 'Task has more than one feed item on insert');
-	}
 
+		$lastFeedItem = array_pop($feed);
 
-	public function testScenarioTrash() {
-		$this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
-	}
-
-	public function testScenarioUntrash() {
-		$this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
+		$this->assertEquals($task->name, $lastFeedItem->focalModelName, "Task name does not match focalModelName in feed");
 	}
 
 	public function testScenarioUpdate() {
-		$this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
+		$task = TaskFactory::insert();
+		$feed = $task->feed;
+		$feedInitialCount = count($feed);
+
+		$task->updateTask(array(
+			'name' => "ptask" . StringUtils::uniqueString(),
+			'startDate' => "08/22/2020",
+			'startTime' => "12:00 pm",
+		));
+
+		$task->refresh();
+
+		$feed = $task->feed;
+
+		$this->assertEquals(2, count($feed) - $feedInitialCount, "The feed was not updated two times when two attributes were changed");
+
 	}
 
 }
