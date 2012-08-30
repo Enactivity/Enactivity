8edb80c46f3ade40477005ba5353cdd5779a0e9e
Ajay Sharma <dudleydragon@hotmail.com>
2012-08-30 11:15:31 -0700

    Code clean up

--- protected/models/UserLoginForm.php
+++ protected/models/UserLoginForm.php
@@ -40,9 +40,9 @@ class UserLoginForm extends CFormModel
 			$this->_identity = new UserIdentity($this->code);
 			$this->_identity->authenticate();
 		}
+		
 		if($this->_identity->errorCode === UserIdentity::ERROR_NONE)
 		{
-			//$duration = $this->rememberMe ? 3600*24*30 : 0; // 30 days
 			$duration = 3600*24*30; // 30 days
 			Yii::app()->user->login($this->_identity, $duration);
 			//TODO: update last login of user
