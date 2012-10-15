e20a90c2cd84224ef0455e1d532936886b5a5223
hvuong <hvuong@poncla.com>
2012-10-14 17:01:01 -0700

    adding views for trash and untrash for email notifications

---
+++ protected/views/mail/task/untrash.php
@@ -0,0 +1,35 @@
+<? 
+/**
+ * View for task model untrash scenario
+ * 
+ */
+
+// calculate article class
+$articleClass = "view";
+$articleClass .= " email";
+
+// start article
+echo PHtml::openTag('article', array(
+	'class' => 'email'
+));
+
+// created date
+echo PHtml::openTag('p');
+echo PHtml::openTag('strong');
+echo PHtml::openTag('time');
+echo PHtml::encode(
+	//FIXME: use actual event time
+	Yii::app()->format->formatDateTime(time())
+);
+echo PHtml::closeTag('time');
+echo PHtml::closeTag('strong');
+echo PHtml::closeTag('p');
+echo PHtml::openTag('p');
+
+//Fantastic! [user] created [taskname] (that starts on [date])?
+//echo "Fantastic! " . PHtml::encode($user->fullName) . " created " . PHtml::encode($changedAttributes['new']) . ".";
+
+echo "Great! " . PHtml::encode($user->fullName) . " restored " . PHtml::link(PHtml::encode($data->name), PHtml::taskURL($data)) . ".";	
+
+echo PHtml::closeTag('article');
+?>
