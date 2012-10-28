e0c19270234edc265655e56720b7efdc4886024b
Ajay Sharma <aj@ajsharma.com>
2012-10-28 15:46:52 -0700

    Stylings

--- protected/views/task/_agenda.php
+++ protected/views/task/_agenda.php
@@ -6,42 +6,29 @@
 
 ?>
 <? foreach($calendar->datedTasks as $day => $times) : ?>
-<? $daytime = strtotime($day); ?>
-	
-<?= PHtml::openTag('h1', array(
-	'id' => PHtml::dateTimeId($daytime),
-	'class' => 'agenda-date',
-)); ?>
-<?= PHtml::encode(Yii::app()->format->formatDate($daytime)); ?>
-<?= PHtml::closeTag('h1'); ?>
+<article class="day">
+	<? $daytime = strtotime($day); ?>
+		
+	<?= PHtml::openTag('h1', array(
+		'id' => PHtml::dateTimeId($daytime),
+		'class' => 'agenda-date',
+	)); ?>
+	<?= PHtml::encode(Yii::app()->format->formatDate($daytime)); ?>
+	<?= PHtml::closeTag('h1'); ?>
 
-<div class="menu">
-	<ol>
-		<li>
-			<?= PHtml::link(
-				'Add Task',
-				array('task/create/',
-					'day' => PHtml::encode(date('d', $daytime)),
-					'month' => PHtml::encode(date('m', $daytime)),
-					'year' => PHtml::encode(date('Y', $daytime)),
-				)
-			); ?>
-		</li>
-	</ol>
-</div>
-
-<? foreach($times as $time => $tasks): ?>
-<? foreach($tasks as $task): ?>
-<?= $this->renderPartial('_view', array(
-	'data'=>$task, 
-	'showParent'=>$showParent,
-)); ?>
-<? endforeach; ?>
-<? endforeach; ?>
+	<? foreach($times as $time => $tasks): ?>
+	<? foreach($tasks as $task): ?>
+	<?= $this->renderPartial('_view', array(
+		'data'=>$task, 
+		'showParent'=>$showParent,
+	)); ?>
+	<? endforeach; ?>
+	<? endforeach; ?>
+</article>
 <? endforeach; ?>
 
 <? if($calendar->hasSomedayTasks) {
-	echo PHtml::openTag('h1', array('id' => 'no-start-datedTasks'));
+	echo PHtml::openTag('h1', array('id' => 'someday-tasks'));
 	echo 'Someday';
 	echo PHtml::closeTag('h1');
 	foreach($calendar->somedayTasks as $task) {
@@ -52,5 +39,4 @@
 			));
 		}
 	}
-}
-?>
+} ?>
