245498904c5e0892eabc00f16ee410b39f0379b0
Ajay Sharma <aj@ajsharma.com>
2012-10-28 14:16:35 -0700

    Moving calendar partial into main calendar view

--- protected/views/task/calendar.php
+++ protected/views/task/calendar.php
@@ -9,15 +9,74 @@ $this->pageTitle = Yii::app()->format->formatMonth($month->firstDayOfMonthTimest
 ?>
 
 <?= PHtml::beginContentHeader(); ?>
-<h1><?= PHtml::encode($this->pageTitle); ?></h1>
+	<h1><?= PHtml::encode($this->pageTitle); ?></h1>
+	<div class="menu">
+		<? $this->widget('zii.widgets.CMenu', array('items'=>array(
+			array(
+				'label'=>PHtml::encode($month->nameOfPreviousMonth), 
+				'url'=>array('task/calendar',
+					'month' => $month->monthIndex - 1 < 1 ? 12 : $month->monthIndex - 1,
+					'year' => $month->monthIndex - 1 < 1 ? $month->year - 1 : $month->year,
+				),
+				'linkOptions'=>array(
+					'id'=>'task-previous-month-menu-item',
+					'class'=>'task-previous-month-menu-item',
+				),
+			),
+			array(
+				'label'=>PHtml::encode($month->nameOfNextMonth), 
+				'url'=>array('task/calendar',
+					'month' => $month->monthIndex + 1 > 12 ? 1 : $month->monthIndex + 1,
+					'year' => $month->monthIndex + 1 > 12 ? $month->year + 1 : $month->year,
+				),
+				'linkOptions'=>array(
+					'id'=>'task-next-month-menu-item',
+					'class'=>'task-next-month-menu-item',
+				),
+			)
+		))); ?>
+	</div>
 <?= PHtml::endContentHeader(); ?>
 
 <section id="calendar-container">
-	<? 
-	// show task calendar
-	echo $this->renderPartial('_calendar', array(
-		'calendar'=>$calendar,
-		'month'=>$month,
-	));
-	?>
+	<article class="calendar">
+		<div class="month">
+			<? while($month->valid()) : ?>
+			<? // if it's the start of a week (Sunday), start a new row ?>
+			<? if($month->currentWDay == 0): ?>
+			<div class="week">
+			<? endif; ?>
+
+				<article class="<?= PHtml::calendarDayClass($month, $calendar); ?>">
+					<header>
+						<?= PHtml::encode($month->currentMDay); ?>
+						<span class="weekday-name"><?= PHtml::encode($month->currentWeekdayShorthand); ?><span>
+					</header>
+					<? foreach ($calendar->getTasksByDate($month->currentDate) as $times): ?>
+					<? foreach ($times as $task): ?>
+					<article class="task">
+						<time>
+							<?= PHtml::encode(Yii::app()->format->formatTime($task->starts)); ?>
+						</time>
+						<h1>
+							<?= PHtml::link(
+								PHtml::encode($task->shortName), 
+								array('/task/view', 'id'=>$task->id)
+							); ?>
+						</h1>
+					</article>
+					<? endforeach; ?>
+					<? endforeach; ?>
+				</article>
+
+				<? // if it's the end of a week, end the row ?>
+			<? if($month->currentWDay == 6) : ?>
+			</div>
+			<? endif; ?>
+
+			<? // iterate ?>
+			<? $month->next(); ?>
+			<? endwhile; ?>
+		</div>
+	</article>
 </section>
