dc1226326bb0fb48412a0a7fa0aeab6af190a006
ponclarobot <team+robot@poncla.com>
2012-10-29 15:59:00 -0700

    Fixing config route

--- protected/config.rb
+++ protected/config.rb
@@ -2,7 +2,7 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "../../www/stylesheets"
+css_dir = "../www/stylesheets"
 sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "javascripts"
