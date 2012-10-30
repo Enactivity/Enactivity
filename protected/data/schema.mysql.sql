5ce26b01d2c3f2ed4a502529f3a258e335204f13
Ajay Sharma <aj@ajsharma.com>
2012-10-30 12:05:05 -0700

    Updating base schema file

--- protected/data/schema.mysql.sql
+++ protected/data/schema.mysql.sql
@@ -1,22 +1,16 @@
 -- phpMyAdmin SQL Dump
--- version 3.1.3.1
+-- version 3.3.10.4
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: Jul 29, 2011 at 07:42 PM
--- Server version: 5.1.33
--- PHP Version: 5.2.9
+-- Host: mysql.alpha.poncla.com
+-- Generation Time: Oct 30, 2012 at 12:04 PM
+-- Server version: 5.1.53
+-- PHP Version: 5.2.17
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
 --
--- Database: `poncla_yii`
+-- Database: `poncla_alpha`
 --
 
 -- --------------------------------------------------------
@@ -30,6 +24,7 @@ CREATE TABLE IF NOT EXISTS `activerecordlog` (
   `groupId` int(11) unsigned NOT NULL,
   `focalModel` varchar(45) NOT NULL,
   `focalModelId` int(11) unsigned NOT NULL,
+  `focalModelName` text NOT NULL,
   `model` varchar(45) NOT NULL,
   `modelId` int(11) unsigned NOT NULL,
   `action` varchar(20) DEFAULT NULL,
@@ -42,7 +37,63 @@ CREATE TABLE IF NOT EXISTS `activerecordlog` (
   PRIMARY KEY (`id`),
   KEY `groupId` (`groupId`),
   KEY `userId` (`userId`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=154 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=85 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart_item`
+--
+
+CREATE TABLE IF NOT EXISTS `cart_item` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `userId` int(10) unsigned NOT NULL,
+  `productType` varchar(255) NOT NULL,
+  `productId` int(10) unsigned NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `purchased` datetime DEFAULT NULL,
+  `delivered` datetime DEFAULT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart_item_custom_field`
+--
+
+CREATE TABLE IF NOT EXISTS `cart_item_custom_field` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `cartItemId` int(10) unsigned NOT NULL,
+  `key` varchar(50) NOT NULL,
+  `value` varchar(255) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `cartItemId` (`cartItemId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comment`
+--
+
+CREATE TABLE IF NOT EXISTS `comment` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `groupId` int(11) unsigned NOT NULL,
+  `creatorId` int(11) unsigned NOT NULL,
+  `model` varchar(45) NOT NULL,
+  `modelId` int(11) unsigned NOT NULL,
+  `content` varchar(4000) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `creatorId` (`creatorId`),
+  KEY `groupId` (`groupId`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=13 ;
 
 -- --------------------------------------------------------
 
@@ -53,27 +104,12 @@ CREATE TABLE IF NOT EXISTS `activerecordlog` (
 CREATE TABLE IF NOT EXISTS `group` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
-  `slug` varchar(50) NOT NULL,
+  `facebookId` varchar(255) DEFAULT NULL,
   `created` datetime DEFAULT NULL,
   `modified` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `name` (`name`),
-  UNIQUE KEY `slug` (`slug`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `group_profile`
---
-
-CREATE TABLE IF NOT EXISTS `group_profile` (
-  `groupId` int(11) unsigned NOT NULL,
-  `description` varchar(4000) DEFAULT NULL,
-  `created` datetime DEFAULT NULL,
-  `modified` datetime DEFAULT NULL,
-  PRIMARY KEY (`groupId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  UNIQUE KEY `facebookId` (`facebookId`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=38 ;
 
 -- --------------------------------------------------------
 
@@ -92,7 +128,27 @@ CREATE TABLE IF NOT EXISTS `group_user` (
   UNIQUE KEY `groupId_2` (`groupId`,`userId`),
   KEY `groupId` (`groupId`),
   KEY `userId` (`userId`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=57 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sweater`
+--
+
+CREATE TABLE IF NOT EXISTS `sweater` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `style` varchar(20) NOT NULL,
+  `clothColor` varchar(50) NOT NULL,
+  `letterColor` varchar(50) NOT NULL,
+  `stitchingColor` varchar(50) NOT NULL,
+  `size` varchar(20) NOT NULL,
+  `available` tinyint(1) NOT NULL DEFAULT '0',
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `style` (`style`,`clothColor`,`letterColor`,`stitchingColor`,`size`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -110,6 +166,8 @@ CREATE TABLE IF NOT EXISTS `task` (
   `lft` int(11) unsigned NOT NULL,
   `rgt` int(11) unsigned NOT NULL,
   `level` int(11) unsigned NOT NULL,
+  `participantsCount` int(11) NOT NULL DEFAULT '0',
+  `participantsCompletedCount` int(11) NOT NULL DEFAULT '0',
   `created` datetime NOT NULL,
   `modified` datetime NOT NULL,
   PRIMARY KEY (`id`),
@@ -118,7 +176,7 @@ CREATE TABLE IF NOT EXISTS `task` (
   KEY `rgt` (`rgt`),
   KEY `level` (`level`),
   KEY `root` (`rootId`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=37 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=24 ;
 
 -- --------------------------------------------------------
 
@@ -138,7 +196,19 @@ CREATE TABLE IF NOT EXISTS `task_user` (
   UNIQUE KEY `userId_taskId` (`userId`,`taskId`),
   KEY `userId` (`userId`),
   KEY `taskId` (`taskId`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=11 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=17 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_migration`
+--
+
+CREATE TABLE IF NOT EXISTS `tbl_migration` (
+  `version` varchar(255) NOT NULL,
+  `apply_time` int(11) DEFAULT NULL,
+  PRIMARY KEY (`version`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -150,22 +220,37 @@ CREATE TABLE IF NOT EXISTS `user` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `email` varchar(50) NOT NULL,
   `token` varchar(40) NOT NULL,
-  `password` varchar(40) DEFAULT NULL,
   `firstName` varchar(50) DEFAULT NULL,
   `lastName` varchar(50) DEFAULT NULL,
+  `phoneNumber` varchar(50) DEFAULT NULL,
   `timeZone` varchar(20) NOT NULL DEFAULT 'America/Los_Angeles',
   `status` varchar(15) NOT NULL DEFAULT 'Pending',
+  `facebookId` varchar(255) DEFAULT NULL,
+  `isAdmin` tinyint(1) NOT NULL DEFAULT '0',
   `created` datetime DEFAULT NULL,
   `modified` datetime DEFAULT NULL,
   `lastLogin` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`),
   UNIQUE KEY `token` (`token`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=8 ;
 
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `cart_item_custom_field`
+--
+ALTER TABLE `cart_item_custom_field`
+  ADD CONSTRAINT `cart_item_cffk_1` FOREIGN KEY (`cartItemId`) REFERENCES `cart_item` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `comment`
+--
+ALTER TABLE `comment`
+  ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`groupId`) REFERENCES `group` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`creatorId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `task`
