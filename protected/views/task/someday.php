cad82b34999cb0740470fb2a74976f93ebe8d3dc
Ajay Sharma <aj@ajsharma.com>
2012-11-04 15:58:42 -0800

    Better blurb

--- protected/views/task/someday.php
+++ protected/views/task/someday.php
@@ -22,7 +22,7 @@ $this->pageTitle = 'Someday';
 	else {
 		//TODO: make more user-friendly
 		echo PHtml::openTag('p', array('class'=>'no-results-message blurb'));
-		echo 'You haven\'t signed up for any tasks.  Why not check out the ';
+		echo 'Nothing here.  Why not check out the ';
 		echo PHtml::link('calendar', array('task/calendar'));
 		echo ' to see what is listed or ';
 		echo PHtml::link('create a new task', array('task/create'));
