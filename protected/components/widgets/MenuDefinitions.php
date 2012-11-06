6596d45b4f52ddaf94beba45764a09efaa222559
Ajay Sharma <aj@ajsharma.com>
2012-11-06 12:07:24 -0800

    adding icon to sync with facebook

--- protected/components/widgets/MenuDefinitions.php
+++ protected/components/widgets/MenuDefinitions.php
@@ -21,7 +21,7 @@ class MenuDefinitions extends CComponent {
 	public static function applicationMenu() {
 		return array(
 			array(
-				'label'=>'<i class="icon"></i> Next', 
+				'label'=>'<i></i> Next', 
 				'linkOptions'=>array(
 					'id'=>'application-navigation-task-index',
 				),
@@ -29,7 +29,7 @@ class MenuDefinitions extends CComponent {
 				'visible'=>!Yii::app()->user->isGuest
 			),
 			array(
-				'label'=>'<i class="icon"></i> Calendar',
+				'label'=>'<i></i> Calendar',
 				'linkOptions'=>array(
 					'id'=>'application-navigation-task-calendar',
 				),
@@ -37,7 +37,7 @@ class MenuDefinitions extends CComponent {
 				'visible'=>!Yii::app()->user->isGuest,
 			),
 			array(
-				'label'=>'<i class="icon"></i> Groups', 
+				'label'=>'<i></i> Groups', 
 				'linkOptions'=>array(
 					'id'=>'application-navigation-membership-index',
 				),
@@ -45,7 +45,7 @@ class MenuDefinitions extends CComponent {
 				'visible'=>!Yii::app()->user->isGuest
 			),
 			array(
-				'label'=>'<i class="icon"></i> New',
+				'label'=>'<i></i> New',
 				'linkOptions'=>array(
 					'id'=>'application-navigation-task-create',
 				),
@@ -61,7 +61,7 @@ class MenuDefinitions extends CComponent {
 				'visible'=>Yii::app()->user->isAdmin
 			),
 			array(
-				'label'=>'<i class="icon"></i> Sign in with Facebook',
+				'label'=>'<i></i> Sign in with Facebook',
 				'linkOptions'=>array(
 					'id'=>'application-navigation-facebook-login',
 				),
