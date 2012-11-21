ef2945c3b4d168ccb2ea80090aa0e981ce3027d4
Ajay Sharma <aj@ajsharma.com>
2012-11-21 11:30:16 -0800

    Updated response to work with new ARLbehavior

--- protected/models/Response.php
+++ protected/models/Response.php
@@ -75,7 +75,12 @@ class Response extends ActiveRecord implements EmailableRecord, LoggableRecord
 			'ActiveRecordLogBehavior'=>array(
 				'class' => 'ext.behaviors.ActiveRecordLogBehavior',
 				'scenarios' => array(
-					// FIXME: implement
+					self::SCENARIO_SIGN_UP => array(),
+					self::SCENARIO_START => array(),
+					self::SCENARIO_QUIT => array(),
+					self::SCENARIO_STOP => array(),
+					self::SCENARIO_COMPLETE => array(),
+					self::SCENARIO_RESUME => array(),
 				),
 			),
 			'EmailNotificationBehavior'=>array(
