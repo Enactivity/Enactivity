99eb4853e54b016c8a5e72ac1e5d9df9f4e1457d
Ajay Sharma <aj@ajsharma.com>
2012-11-25 19:46:45 -0800

    Adding url funcitons

--- protected/views/comment/_view.php
+++ protected/views/comment/_view.php
@@ -1,34 +1,29 @@
-<? 
-/**
- * View for individual comments
- * @uses $data Comment model
- */
-?>
-<article id="comment-<?= PHtml::encode($data->id); ?>" class="<?= PHtml::commentClass($data); ?>">
+{{! @uses $comment Comment model }}
+<article id="comment-<?= PHtml::encode($comment->id); ?>" class="<?= PHtml::commentClass($comment); ?>">
 	<div class="avatar">
-		<?= PHtml::image($data->creator->pictureUrl); ?>
+		<?= PHtml::image($comment->creator->pictureUrl); ?>
 	</div>
 	<header>
 		<h1 class="creator">
 			<? //author 
 				$this->widget('application.components.widgets.UserLink', array(
-				'userModel' => $data->creator,
+				'userModel' => $comment->creator,
 			));  ?>
 		</h1>
 		<span class="created">@
 			<? if(isset($model)) : ?>
-			<?= PHtml::encode(Yii::app()->format->formatDateTimeAsAgo(strtotime($data->created))); ?>
+			<?= PHtml::encode(Yii::app()->format->formatDateTimeAsAgo(strtotime($comment->created))); ?>
 			<? else: ?>
 			<?= PHtml::link(
-				PHtml::encode(Yii::app()->format->formatDateTimeAsAgo(strtotime($data->created))),
+				PHtml::encode(Yii::app()->format->formatDateTimeAsAgo(strtotime($comment->created))),
 				array(Yii::app()->request->pathInfo, 
-					'#' => 'comment-' . $data->id,
+					'#' => 'comment-' . $comment->id,
 				)
 			); ?>
 		<? endif; ?>
 		</span>
 	</header>
 
-	<?= Yii::app()->format->formatStyledText($data->content); ?>
+	<?= Yii::app()->format->formatStyledText($comment->content); ?>
 	
 </article>
