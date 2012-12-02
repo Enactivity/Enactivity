9cdd43f084b91b1069e4ba1c86b6656510ba4b70
Ajay Sharma <aj@ajsharma.com>
2012-12-02 15:01:22 -0800

    Proper ajax support, with CSRF protecton & logless ajax response

--- protected/views/task/_view.php
+++ protected/views/task/_view.php
@@ -3,11 +3,7 @@
  * View for individual task models
  * 
  * @param Task $data model
- * @param boolean $showParent
  */
-
-$showParent = isset($showParent) ? $showParent : true;
-
 $story = $this->beginWidget('application.components.widgets.Story', array(
 	'htmlOptions'=>array(
 		'id'=>"task-" . PHtml::encode($data->id),
@@ -29,8 +25,139 @@ $story = $this->beginWidget('application.components.widgets.Story', array(
 		</h1>
 	<? $story->endStoryContent(); ?>
 
-	<?= $this->renderPartial('/task/_controls', array(
-		'model'=>$data,
-		'response'=>$data->currentresponse,
-	)); ?>
+	<div class="menu controls">
+	<ul>
+		<? if($data->currentresponse->canSignUp): ?>
+		<li> 
+			<?= PHtml::ajaxButton(
+				"I'll do this",
+				$data->signUpUrl,
+				array( //ajax
+					'replace' =>"#task-" . PHtml::encode($data->id), 
+					'type'=>'POST',
+					'data'=>array(
+						Yii::app()->request->csrfTokenName => Yii::app()->request->csrfToken,
+					),
+				),
+				array( //html
+					'id'=>'task-sign-up-menu-item-' . $data->id,
+					'name'=>'task-sign-up-menu-item-' . $data->id,
+					'class'=>'positive task-sign-up-menu-item',
+					'title'=>'Sign up for task',
+				)
+			); ?>
+		</li>
+		<? endif; ?>
+
+		<? if($data->currentresponse->canStart): ?>
+		<li>
+			<?= PHtml::ajaxButton(
+				"I'm doing this", 
+				$data->startUrl,
+				array( //ajax
+					'replace' =>"#task-" . PHtml::encode($data->id), 
+					'type'=>'POST',
+					'data'=>array(
+						Yii::app()->request->csrfTokenName => Yii::app()->request->csrfToken,
+					),
+				),
+				array( //html
+					'id'=>'task-start-menu-item-' . $data->id,
+					'name'=>'task-start-menu-item-' . $data->id,
+					'class'=>'positive task-start-menu-item',
+					'title'=>'Show that you\'ve started working on this task',
+				)
+			); ?>
+		</li>
+		<? endif; ?>
+
+		<? if($data->currentresponse->canComplete): ?>
+		<li>
+			<?= PHtml::ajaxButton(
+				"I've done this",
+				$data->completeUrl,
+				array( //ajax
+					'replace' =>"#task-" . PHtml::encode($data->id), 
+					'type'=>'POST',
+					'data'=>array(
+						Yii::app()->request->csrfTokenName => Yii::app()->request->csrfToken,
+					),
+				),
+				array( //html
+					'id'=>'task-complete-menu-item-' . $data->id,
+					'name'=>'task-complete-menu-item-' . $data->id,
+					'class'=>'positive task-complete-menu-item',
+					'title'=>'Finish working on this task',
+				)
+			); ?>
+		</li>
+		<? endif; ?>
+
+		<? if($data->currentresponse->canResume): ?>
+		<li>
+			<?= PHtml::ajaxButton(
+				"I've got more to do",
+				$data->resumeUrl,
+				array( //ajax
+					'replace' =>"#task-" . PHtml::encode($data->id), 
+					'type'=>'POST',
+					'data'=>array(
+						Yii::app()->request->csrfTokenName => Yii::app()->request->csrfToken,
+					),
+				),
+				array( //html
+					'id'=>'task-resume-menu-item-' . $data->id,
+					'name'=>'task-resume-menu-item-' . $data->id,
+					'class'=>'neutral task-resume-menu-item',
+					'title'=>'Resume work on this task',
+				)
+			); ?>
+		</li>
+		<? endif; ?>
+
+		<? if($data->currentresponse->canQuit): ?>
+		<li>
+			<?= PHtml::ajaxButton(
+				"Quit", 
+				$data->quitUrl,
+				array( //ajax
+					'replace' =>"#task-" . PHtml::encode($data->id), 
+					'type'=>'POST',
+					'data'=>array(
+						Yii::app()->request->csrfTokenName => Yii::app()->request->csrfToken,
+					),
+				),
+				array( //html
+					'id'=>'task-quit-menu-item-' . $data->id,
+					'name'=>'task-quit-menu-item-' . $data->id,
+					'class'=>'neutral task-quit-menu-item',
+					'title'=>'Quit this task',
+				)
+			); ?>
+		</li>
+		<? endif; ?>
+
+		<? if($data->currentresponse->canIgnore): ?>
+		<li>
+			<?= PHtml::ajaxButton(
+				"Ignore", 
+				$data->ignoreUrl,
+				array( //ajax
+					'replace' =>"#task-" . PHtml::encode($data->id), 
+					'type'=>'POST',
+					'data'=>array(
+						Yii::app()->request->csrfTokenName => Yii::app()->request->csrfToken,
+					),
+				),
+				array( //html
+					'id'=>'task-ignore-menu-item-' . $data->id,
+					'name'=>'task-ignore-menu-item-' . $data->id,
+					'class'=>'neutral task-ignore-menu-item',
+					'title'=>'Ignore this task',
+				)
+			); ?>
+		</li>
+		<? endif; ?>
+	</ul>
+</div>
 <? $this->endWidget(); ?>
