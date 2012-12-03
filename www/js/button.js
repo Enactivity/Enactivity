46545cebb8aacd1f2cc2d48322058e2ed4ce13df
Ajay Sharma <aj@ajsharma.com>
2012-12-02 17:23:30 -0800

    HTML5 esque ajax buttons

--- www/js/button.js
+++ www/js/button.js
@@ -1,29 +1,33 @@
 /** 
  * Unobtrusive formless submit button
- * @requires jquery.yii.js for submitting
+ * @requires jquery.ajax for submitting
 **/
 $(function() {
 
 	"use strict";
 
-	var buttonSelector = '[data-submit-to]'; // or ':button' could work
+	var buttonSelector = '[data-ajax-url]'; // or ':button' could work
 
 	/**
 	 * Add button behavior to selected items.
-	 * @param {String} selector
+	 * @param {String} selector for which elements to apply the button listener to
 	 * @return {JQuery} see http://api.jquery.com/each/
 	 */
-	$.fn.button = function(selector) {
+	$.fn.enactivityAjaxButton = function(selector) {
 		return this.each(function() {
 			$(this).on('click', selector || buttonSelector, function() {
 				var button = $(this);
-				jQuery.yii.submitForm(
-					this,
-					button.data("submit-to"),
-					{
-						'YII_CSRF_TOKEN':$("meta[name=YII_CSRF_TOKEN]").attr("content")
+				$.ajax({
+					'type':'POST',
+					'data':{
+						'YII_CSRF_TOKEN':$("meta[name=YII_CSRF_TOKEN]").attr("content") // TODO: replace hardcoded YII_CSRF_TOKEN name
+					},
+					'url':button.data('ajax-url'),
+					'cache':false,
+					'success':function(responseHtml) {
+						$(button.data('container-id')).replaceWith(responseHtml);
 					}
-				);
+				});
 				return false; /* prevent bubbles */
 			});
 		});
@@ -31,6 +35,6 @@ $(function() {
 
 	/* Apply dropdown to html elements */
 	$(function() {
-		$('body').button(buttonSelector);
+		$('body').enactivityAjaxButton(buttonSelector);
 	});
 });
