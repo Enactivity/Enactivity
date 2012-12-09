4f43ddf1732e1466f261a61494011b12a5ae70b9
Ajay Sharma <aj@ajsharma.com>
2012-12-09 13:44:34 -0800

    Refactoring clear button js to separate file

--- protected/javascripts/ClearInputsButton.js
+++ protected/javascripts/ClearInputsButton.js
@@ -1,80 +1,45 @@
-$(document).ready(function(){
-	$('.clear-date-time').click(function(event){
-		$(this).siblings('input').val("");
-		$(this).siblings('select').val("");
-		return false;
-	});
-});
-
+/**
+ * Creates a button that will clear it's siblings' inputs 
+ * Requires two data attributes:
+ * @param 'data-type=[clear-button]' to innately apply click functionality
+ * @param 'data-inputs' selector or array of selectors that clicking the button should clear
+ *   if array, be sure to use double quotes: ["#id1","#id2"]
+ **/
 $(function() {
 
 	"use strict";
 
-	var buttonSelector = '[data-type-clear-button]';
+	var buttonSelector = "[data-type=clear-button]";
+
+	var clearInput = function(selector) {
+		$(selector).val("");
+	}
 
 	/**
 	 * Add button behavior to selected items.
 	 * @param {String} selector for which elements to apply the button listener to
 	 * @return {JQuery} see http://api.jquery.com/each/
 	 */
-	$.fn.ClearButton = function(selector) {
+	$.fn.ClearInputsButton = function(selector) {
 		return this.each(function() {
 			$(this).on('click', selector || buttonSelector, function() {
 				var button = $(this);
-				$.ajax({
-					'type':'POST',
-					'data':{
-						'YII_CSRF_TOKEN':button.data('csrf-token') || $("meta[name=YII_CSRF_TOKEN]").attr("content") // TODO: replace hardcoded YII_CSRF_TOKEN name
-					},
-					'url':button.data('ajax-url'),
-					'cache':false,
-					'success':function(responseHtml) {
-						$(button.data('container-id')).replaceWith(responseHtml);
-					}
-				});
+				var inputs = button.data('inputs');
+				if($.isArray(inputs)) {
+					for (var i = inputs.length - 1; i >= 0; i--) {
+						clearInput(inputs[i]);
+					};
+				}
+				else {
+					clearInput(inputs);
+				}
 				return false; /* prevent bubbles */
 			});
 		});
 	};
 
-});
-
-/** 
- * Clear inputs button
-**/
-$(function() {
-
-	"use strict";
-
-	var buttonSelector = '[data-ajax-url]'; // or ':button' could work
-
-	/**
-	 * Add button behavior to selected items.
-	 * @param {String} selector for which elements to apply the button listener to
-	 * @return {JQuery} see http://api.jquery.com/each/
-	 */
-	$.fn.AjaxButton = function(selector) {
-		return this.each(function() {
-			$(this).on('click', selector || buttonSelector, function() {
-				var button = $(this);
-				$.ajax({
-					'type':'POST',
-					'data':{
-						'YII_CSRF_TOKEN':button.data('csrf-token') || $("meta[name=YII_CSRF_TOKEN]").attr("content") // TODO: replace hardcoded YII_CSRF_TOKEN name
-					},
-					'url':button.data('ajax-url'),
-					'cache':false,
-					'success':function(responseHtml) {
-						$(button.data('container-id')).replaceWith(responseHtml);
-					}
-				});
-				return false; /* prevent bubbles */
-			});
-		});
-	};
-
-	/* Apply dropdown to html elements */
+	/* Apply button to html elements */
 	$(function() {
-		$('body').AjaxButton(buttonSelector);
+		$('body').ClearInputsButton(buttonSelector);
 	});
 });
