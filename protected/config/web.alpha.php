4af3aed76d55250063e0ad3dc80c8a9d5a044387
Ajay Sharma <aj@ajsharma.com>
2012-12-30 16:49:44 -0800

    Updating alpha db for new hostname

--- protected/config/web.alpha.php
+++ protected/config/web.alpha.php
@@ -9,7 +9,7 @@ return CMap::mergeArray(
 			
 			// MySQL database settings for alpha
 			'db'=>array(
-				'connectionString' => 'mysql:host=mysql.alpha.poncla.com;dbname=poncla_alpha',
+				'connectionString' => 'mysql:host=mysql.ajsharma.dev.enactivity.com;dbname=poncla_alpha',
 				'username' => 'poncla_alpha',
 				'password' => 'alpha123',
 			),
