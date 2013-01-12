0f45f8be5d0943163e87af26bdfc36c867295209
Ajay Sharma <aj@ajsharma.com>
2013-01-11 16:35:41 -0800

    Moving to tables

--- protected/views/admin/registrations.php
+++ protected/views/admin/registrations.php
@@ -1,13 +1,15 @@
 <section class="registrations content">
 	<p class="blurb">Ordered by registration (newest to oldest).</p>
 
-	<ol>
-		<? foreach($userDataProvider->data as $user): ?>
-		<li>
-			<?= PHtml::encode($user->created); ?>
-			<?= PHtml::encode($user->fullName); ?>
-			<?= PHtml::mailto(PHtml::encode($user->email), PHtml::encode($user->email)); ?>
-		</li>
-		<? endforeach; ?>
-	</ol>
+	<table>
+		<tbody>
+			<? foreach($userDataProvider->data as $user): ?>
+			<tr>
+				<td><?= PHtml::encode($user->created); ?></td>
+				<td><?= PHtml::encode($user->fullName); ?></td>
+				<td><?= PHtml::mailto(PHtml::encode($user->email), PHtml::encode($user->email)); ?></td>
+			</tr>
+			<? endforeach; ?>
+		</tbody>
+	</table>
 </section>
