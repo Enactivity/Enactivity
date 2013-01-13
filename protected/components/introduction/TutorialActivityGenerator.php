a4d78b424093d68a6ae00c783f15022488833167
hvuong <hvuong@poncla.com>
2013-01-13 00:07:50 -0800

    adding better comments

--- protected/components/introduction/TutorialActivityGenerator.php
+++ protected/components/introduction/TutorialActivityGenerator.php
@@ -7,7 +7,7 @@ class TutorialActivityGenerator extends CComponent
 		$activityAttributes = array(
 			/*fix me: should be groupless*/
 			'groupId' => 10,
-			'name' => 'Learn how to use Enactivity222222',
+			'name' => 'Banana2',
 			'description' => 'Welcome to Enactivity! This is a sample activity to help guide you through the process of creating, sharing, and participating in activities and tasks.',
 		);
 
@@ -34,23 +34,23 @@ class TutorialActivityGenerator extends CComponent
 		Response::start($form->tasks[0]->id, $user->id);
 		Response::complete($form->tasks[0]->id, $user->id);
 		$signComment = new Comment();
-		$signComment ->publishComment($form->tasks[0], array('content' => 'This is signing up'));
+		$signComment ->publishComment($form->tasks[0], array('content' => 'Sign up for an Activity'));
 
 		//Setting respones for Read about Enactivity
 		Response::signUp($form->tasks[1]->id, $user->id);
 		Response::start($form->tasks[1]->id, $user->id);
 		$readComment = new Comment();
-		$readComment->publishComment($form->tasks[1], array('content' => 'This is signing up'));
+		$readComment->publishComment($form->tasks[1], array('content' => 'Read more about us!'));
 
 		//Setting respones for Create a new Activity		
 		Response::signUp($form->tasks[2]->id, $user->id);
 		$newActivityComment = new Comment();
-		$newActivityComment->publishComment($form->tasks[2], array('content' => 'This is signing up'));
+		$newActivityComment->publishComment($form->tasks[2], array('content' => 'Creating an Activity'));
 
 		//Setting respones for Create a new Task
 		Response::signUp($form->tasks[3]->id, $user->id);
 		$newTaskComment = new Comment();
-		$newTaskComment->publishComment($form->tasks[3], array('content' => 'This is signing up'));
+		$newTaskComment->publishComment($form->tasks[3], array('content' => 'Creating a Task'));
 	}
 	
 }
