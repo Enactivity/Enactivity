8d33e0893314161919b1ecbb14b4a910b8125664
Ajay Sharma <aj@ajsharma.com>
2013-01-14 11:30:57 -0800

    Use app name

--- protected/components/introduction/TutorialActivityGenerator.php
+++ protected/components/introduction/TutorialActivityGenerator.php
@@ -5,16 +5,16 @@ class TutorialActivityGenerator extends CComponent
 	public static function generateIntroActivity($userId)
 	{
 		$activityAttributes = array(
-			'name' => "Meet {Yii::app()->name}",
-			'description' => "Welcome to {Yii::app()->name}! This is a sample activity to help guide you through the process of creating, sharing, and participating in activities and tasks.",
+			'name' => "Meet {${Yii::app()->name}}",
+			'description' => "Welcome to {${Yii::app()->name}}! This is a sample activity to help guide you through the process of creating, sharing, and participating in activities and tasks.",
 		);
 
 		$tasksAttributesList = array(
 			array(
-				'name' => 'Sign up for {Yii::app()->name}',
+				'name' => 'Sign up for {${Yii::app()->name}}',
 			),
 			array(
-				'name' => 'Read about {Yii::app()->name}',
+				'name' => 'Read about {${Yii::app()->name}}',
 			),
 			array(
 				'name' => 'Create a new Activity',
@@ -27,25 +27,25 @@ class TutorialActivityGenerator extends CComponent
 		$form = new ActivityAndTasksForm();
 		$form->publishWithoutGroup($activityAttributes, $tasksAttributesList);
 
-		//Setting responses for Sign up
+		// Setting responses for Sign up
 		Response::signUp($form->tasks[0]->id, $userId);
 		Response::start($form->tasks[0]->id, $userId);
 		Response::complete($form->tasks[0]->id, $userId);
 		$signComment = new Comment();
 		$signComment ->publishComment($form->tasks[0], array('content' => 'Sign up for an Activity'));
 
-		//Setting respones for Read about
+		// Setting respones for Read about
 		Response::signUp($form->tasks[1]->id, $userId);
 		Response::start($form->tasks[1]->id, $userId);
 		$readComment = new Comment();
 		$readComment->publishComment($form->tasks[1], array('content' => 'Read more about us!'));
 
-		//Setting respones for Create a new Activity		
+		// Setting respones for Create a new Activity
 		Response::pend($form->tasks[2]->id, $userId);
 		$newActivityComment = new Comment();
 		$newActivityComment->publishComment($form->tasks[2], array('content' => 'Creating an Activity'));
 
-		//Setting respones for Create a new Task
+		// Setting respones for Create a new Task
 		Response::pend($form->tasks[3]->id, $userId);
 		$newTaskComment = new Comment();
 		$newTaskComment->publishComment($form->tasks[3], array('content' => 'Creating a Task'));
