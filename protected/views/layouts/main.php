f3e6b73880fb3380a9d711905843602f456e2013
Ajay Sharma <aj@ajsharma.com>
2013-01-13 17:30:40 -0800

    Adding max scale to help robust no-scale

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -2,7 +2,7 @@
 <html>
 	<head>
 		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no"/> 
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/>
 		<meta name="<?= Yii::app()->request->csrfTokenName; ?>" content="<?= Yii::app()->request->csrfToken; ?>">
 		<!-- Add "maximum-scale=1" to fix the weird iOS auto-zoom bug on orientation changes. -->
 
