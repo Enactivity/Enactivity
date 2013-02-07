17a19cf44b0010611e7701e37bb7af134808ae12
Ajay Sharma <aj@ajsharma.com>
2013-02-07 12:40:22 -0800

    replace ip address with actual server address

--- protected/config/production.php
+++ protected/config/production.php
@@ -7,7 +7,7 @@ return array(
 			
 			// MySQL database settings for production
 			'db'=>array(
-				'connectionString' => 'mysql:dbname=enactivity_production;host=173.236.204.211',
+				'connectionString' => 'mysql:dbname=enactivity_production;host=production.enactivity.com',
 				'password' => '1f3870be274f6c4',
 				'username' => 'poncla_live',
 			),
