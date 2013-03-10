d8dd7488b3fb29b8f089e501634d10795205eabd
Ajay Sharma <aj@ajsharma.com>
2013-03-10 14:13:20 -0700

    blurbify text

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -2,7 +2,7 @@
 	<div class="inner">
 		<h1><span class="big-e">E</span>nactivity</h1>
 
-		<h2>A new way to organize activities with your group.</h2>
+		<h2>A good way to organize activities with your group.</h2>
 
 		<p>Enactivity makes it easy to plan activities that people 
 			participate in, not just attend.</p>
@@ -15,40 +15,48 @@
 	<div class="inner">
 		<blockquote>
 			<p>What a great app, everyone should be using it!</p>
-			<p class="cite"><cite>say the guys who made Enactivity</cite></p>
+			<p class="cite"><cite>say the guys who made it.</cite></p>
 		</blockquote>
 	</div>
 </section>
 
 <section class="feature describe-responsive">
 	<div class="inner">
-		<h2><i></i> Works Everywhere</h2>
-		<p>Enactivity's powerful web app works in all modern browsers, be they mobile devices, tablets, desktops, or laptops.</p>
-		<p>If you've got an internet connection, you've got Enactivity.</p>
+		<div class="feature-blurb">
+			<h2><i></i> Works Everywhere You Are</h2>
+			<p>Enactivity's powerful web app works in all modern browsers, be they mobile devices, tablets, desktops, or laptops.</p>
+			<p>If you've got an internet connection, you've got Enactivity.</p>
+		</div>
 	</div>
 </section>
 
 <section class="feature describe-dashboard">
 	<div class="inner">
-		<h2><i></i> Simple Dashboard</h2>
-		<p>Enactivity's dashboard shows the tasks you've signed up for and those still waiting for your response.</p>
-		<p>Never fall behind again.</p>
+		<div class="feature-blurb">
+			<h2><i></i> Simple Dashboard</h2>
+			<p>Enactivity's dashboard shows the tasks you've signed up for and those still waiting for your response.</p>
+			<p>Never fall behind again.</p>
+		</div>
 	</div>
 </section>
 
 <section class="feature describe-calendar">
 	<div class="inner">
-		<h2><i></i> Calendar</h2>
-		<p>All your groups' activities can be browsed at a glance on Enactivity's calendar.</p>
-		<p>Stay organized.</p>
+		<div class="feature-blurb">
+			<h2><i></i> Built-in Calendar</h2>
+			<p>All your groups' activities can be browsed at a glance on Enactivity's calendar.</p>
+			<p>Stay organized.</p>
+		</div>
 	</div>
 </section>
 
 <section class="feature describe-facebook">
 	<div class="inner">
-		<h2><i></i> Facebook Integration</h2>
-		<p>Enactivity works with Facebook, your groups are automatically imported so you can start creating and sharing activities right away.</p>
-		<p>Easy.</p>
+		<div class="feature-blurb">
+			<h2><i></i> Facebook Integration</h2>
+			<p>Enactivity works with Facebook, your groups are automatically imported so you can start creating and sharing activities right away.</p>
+			<p>Easy.</p>
+		</div>
 	</div>
 </section>
 
